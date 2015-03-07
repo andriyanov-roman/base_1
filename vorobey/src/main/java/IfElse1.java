14f01ed901f5922efbdc146372a183e2f3befd54
vorobeyDjack <vrba@i.ua>
2015-03-07 19:48:34 +0200

    do something great!!

---
+++ vorobey/src/main/java/IfElse1.java
@@ -0,0 +1,12 @@
+
+public class IfElse1 {
+    public static void main(String[] args) {
+        int a = 9, b = 41, c = 5;
+        if (a > b && a > c)
+            System.out.println(" a :" + a);
+        else if (a < b && a > c)
+            System.out.println(" b :" + b);
+        else
+            System.out.println(" c :" + c);
+    }
+}
