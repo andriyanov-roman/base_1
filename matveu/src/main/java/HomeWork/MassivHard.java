323a3ffa612ad9c8f14a267cca27564dcfe02f5a
Igor Matviichuk <matveu1990@gmail.com>
2015-03-08 00:09:02 +0200

    1

--- matveu/src/main/java/HomeWork/MassivHard.java
+++ matveu/src/main/java/HomeWork/MassivHard.java
@@ -1,4 +1,25 @@
 package HomeWork;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MassivHard {
+
+    //Пользователь вводит положительное число,программа создает массив указаного размера,в промежутке [-15;15]
+    //Найти сумму всех чисел находящихся между минимальным и максимальным числом в массиве
+
+    public static void main(String [] args) throws IOException {
+        int x;
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        do {
+            System.out.println("Введите положительное число: ");
+            String s = br.readLine();
+            x = Integer.parseInt(s);
+        }while (x<0);
+
+
+
+    }
+
 }
