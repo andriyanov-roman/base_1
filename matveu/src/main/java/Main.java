0b597690ab6d65fbdfb6434af58cf1f554c0e51c
matveu <matveu1990@gmail.com>
2015-03-07 14:21:41 +0200

    change

--- matveu/src/main/java/Main.java
+++ matveu/src/main/java/Main.java
@@ -1,5 +1,19 @@
 /**
  * Created by Администратор on 06.03.2015.
  */
+import java.io.*;
+
 public class Main {
+    public static void main(String [] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите число! ");
+        String s=br.readLine();
+        int x=Integer.parseInt(s);
+        int[] mas=new int[x];
+        for(int i=0;i<mas.length;i++){
+            mas[i]=(int)(Math.random()*21-10);
+            System.out.print(mas[i]);
+        }
+
+    }
 }
