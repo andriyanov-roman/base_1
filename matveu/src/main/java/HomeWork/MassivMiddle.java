b176d9d412dc145be7437dd15d859f1546126b8a
Igor Matviichuk <matveu1990@gmail.com>
2015-03-07 18:43:26 +0200

    2

---
+++ matveu/src/main/java/HomeWork/MassivMiddle.java
@@ -0,0 +1,17 @@
+package HomeWork;
+import java.io.*;
+
+public class MassivMiddle {
+
+    // Вывести в консоль половину массива, среднеарифметическое которых является наибольшим
+
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите четное положительное число: ");
+        String s = br.readLine();
+        int x=Integer.parseInt(s);
+        int[] mas = new int[];
+    }
+}
