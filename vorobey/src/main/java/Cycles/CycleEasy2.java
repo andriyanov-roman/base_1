b767889832f5bc2a35e51c90b419749e0325182f
vorobeyDjack <vrba@i.ua>
2015-03-07 19:35:13 +0200

    only watching/delete comments

---
+++ vorobey/src/main/java/Cycles/CycleEasy2.java
@@ -0,0 +1,12 @@
+package Cycles;
+
+
+public class CycleEasy2 {
+    public static void main(String[] args) {
+        int k = -1;
+        for (int i = 0; i < 55; i++) {
+            k += 2;
+            System.out.print(" " + k);
+        }
+    }
+}
