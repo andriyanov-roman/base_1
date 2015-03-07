2349dc1804c4a524596b7c8c6ae92404903c9748
vorobeyDjack <vrba@i.ua>
2015-03-07 19:44:35 +0200

    do something great!!clock

---
+++ vorobey/src/main/java/Cycles/CycleClockMinutesHours.java
@@ -0,0 +1,16 @@
+package Cycles;
+
+
+public class CycleClockMinutesHours {
+    public static void main(String[] args) {
+        int i, j, count = 0;
+        for (i = 0; i < 24; i++) {
+            for (j = 0; j < 60; j++) {
+                if ((i / 10) == (j % 10) && (i % 10) == (j / 10))
+                    count++;// Количество совпадений
+            }
+        }
+
+
+    }
+}
