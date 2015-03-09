e7615cee9b9ea6dd348d9934668404617da13fe9
vorobeyDjack <vrba@i.ua>
2015-03-09 20:43:55 +0200

    changing method checkUsers

--- vorobey/src/main/java/Main1.java
+++ vorobey/src/main/java/Main1.java
@@ -34,21 +34,28 @@ public class Main1 {
     }
 
     public static void checkUser() {
+        int count = 0;
         User[] users = UsersUtil.getUsers();
-        for (int i = 0; i < users.length; i++) {
+        for (int i = 0; i < 1; i++) {
             for (int j = 0; j < users.length; j++) {
                 if (users[i].name.equals(users[j].secondName)) {
                     System.out.println("Имя и Фамилия совпадают");
+                    System.out.println(users[i].name + "  " + users[i].secondName);
+                    count++;
+                    System.out.println("Количество сотрудников с совпадениями: " + count);
                 } else {
-                    System.out.println("Не совпадают");
-                }
-            }
-            System.out.println();
+                    System.out.println("Имя и Фамилия не совпадают");
+                    System.out.println(users[j].name + "  " + users[j].secondName);
+                    System.out.println("Количество сотрудников с несовпадениями: " + count);
 
+                }
+
+
+            }
 
         }
-
     }
 }
+
 
 
