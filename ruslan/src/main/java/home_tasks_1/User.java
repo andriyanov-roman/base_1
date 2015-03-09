eefd2f615824929b32c7cf5493aace4802918b63
b0gut <ruslanvankevich@gmail.com>
2015-03-09 20:27:04 +0200

    Merge branch 'master' of E:\base_1 with conflicts.

---
+++ ruslan/src/main/java/home_tasks_1/User.java
@@ -0,0 +1,15 @@
+package home_tasks_1;
+
+/**
+ * Created by user on 09.03.2015.
+ */
+public class User {
+    public Long id;
+    public String name;
+    public String secondName;
+    public String login;
+    public String password;
+    public int age;
+
+    }
+
