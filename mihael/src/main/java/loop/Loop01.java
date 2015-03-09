2b359598ae85a4cacb708813c6310fb1bcc8dd7c
job.dmy@gmail.com <dm_admin@online.com.ua>
2015-03-09 16:41:04 +0200

    Mihael commit main

---
+++ mihael/src/main/java/loop/Loop01.java
@@ -0,0 +1,15 @@
+package loop;
+
+/**
+ * Created by Администратор on 09.03.2015.
+ */
+public class Loop01 {
+    public static void main (String[] args){
+        int x = 1000;
+        while (x<10000) {
+            System.out.println(x+" ");
+            x +=3;
+        }
+
+    }
+}
