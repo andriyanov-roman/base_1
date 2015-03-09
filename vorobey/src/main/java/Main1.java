2c418faea35a4fdaa57ede21c1714459a1859bb9
vorobeyDjack <vrba@i.ua>
2015-03-09 19:23:42 +0200

    creating class

---
+++ vorobey/src/main/java/Main1.java
@@ -0,0 +1,8 @@
+/**
+ * Created by user on 09.03.2015.
+ */
+public class Main1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ vorobey/src/main/java/User.java
@@ -0,0 +1,11 @@
+/**
+ * Created by user on 09.03.2015.
+ */
+public class User {
+    public Long id;
+    public String name;
+    public String secondName;
+    public String lohin;
+    public String passsword;
+    public int age;
+}
