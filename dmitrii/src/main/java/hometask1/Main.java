22233a052aa6e44b49badfedf594f4e0d43f22fe
lisunovdv <lisunovdv@gmail.com>
2015-03-09 21:08:54 +0200

    Д-з от 09-03-2015

--- dmitrii/src/main/java/hometask1/Main.java
+++ dmitrii/src/main/java/hometask1/Main.java
@@ -16,6 +16,7 @@ public class Main {
         System.out.println (
                 "\n 0.Show all users"+
                 "\n 1.Show users with equal parametres"+
+                        "\n 2.Show users with equal Names + Surnames"+
                 "\n Exit. End programm." +
                 "\nEnter command key:");
         while (scanner.hasNext()){
@@ -25,6 +26,9 @@ public class Main {
                     break;
                 case "1":
                     showUsersEq();
+                    break;
+                case "2":
+                    showUsersWithEqualNamesAndSurnames();
                     break;
                 case "Exit":
                     System.exit(0);
@@ -44,10 +48,25 @@ public class Main {
     public static void showUsersEq() {
         User[] users = UsersUtil.getUsers();
         for (int i = 0; i < users.length; i++) {
-            if (users[i].login.equals(users[i].name) | users[i].login.equals(users[i].secondName)) {
-                System.out.println("Login "+users[i].login+" Name: "+users[i].name + " Surname:"+users[i].secondName);
+            if (users[i].login.equals(users[i].name) ) {
+                System.out.println("Login Eq Name. Login:"+users[i].login+" / name: "+users[i].name+" (user id="+users[i].id+")");
+            }
+            if ( users[i].login.equals(users[i].secondName)) {
+                System.out.println("Login Eq Surname. Login:"+users[i].login + " Surname:"+users[i].secondName+" (user id="+users[i].id+")");
             }
         }
         System.out.println();
+    }
+
+    public static void showUsersWithEqualNamesAndSurnames(){
+        // Барахло (пересмотреть!!!!!!!!)
+        User[] users = UsersUtil.getUsers();
+        for (int i = 0; i < users.length; i++) {
+            for (int j = 1; j < users.length; j++) {
+                if ( users[i].name.equals(users[j].name) & users[i].secondName.equals(users[j].secondName) ) {
+                    System.out.println ("Duplicates:"+users[i].name+" "+users[i].secondName+" | id="+users[i].id+", id="+users[j].id);
+                }
+            }
+        }
     }
 }
