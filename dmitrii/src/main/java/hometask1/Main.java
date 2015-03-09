f07c6f675aa119149645dcedc41ff788e2989ec1
lisunovdv <lisunovdv@gmail.com>
2015-03-09 19:49:09 +0200

    Д-з от 09-03-2015

--- dmitrii/src/main/java/hometask1/Main.java
+++ dmitrii/src/main/java/hometask1/Main.java
@@ -5,6 +5,14 @@ package hometask1;
  */
 public class Main {
     public static void main(String[] args) {
+        showUsers();
+    }
 
+    public static void showUsers() {
+        User[] users = UsersUtil.getUsers();
+        for (int i = 0; i < users.length; i++) {
+            System.out.print(users[i].name+" "+users[i].secondName);
+        }
+        System.out.println();
     }
 }
