4190e85cc8ddcfb817c4d7296f7673599746c9da
lisunovdv <lisunovdv@gmail.com>
2015-03-09 20:37:51 +0200

    Д-з от 09-03-2015

--- dmitrii/src/main/java/hometask1/UsersUtil.java
+++ dmitrii/src/main/java/hometask1/UsersUtil.java
@@ -21,11 +21,27 @@ public class UsersUtil {
         u2.name = "Ihor";
         u2.secondName = "ihor";
 
+        User u3 = new User();
+        u3.age = 65;
+        u3.id = 3L;
+        u3.login = "Ivan";
+        u3.password = "pass432";
+        u3.name = "Ivan";
+        u3.secondName = "Karman";
+
+        User u4 = new User();
+        u4.age = 32;
+        u4.id = 4L;
+        u4.login = "va2017";
+        u4.password = "pass777";
+        u4.name = "Vasyl";
+        u4.secondName = "Petrov";
+
         //User[] users = new User[2];
         //users[0]= u1;
         //users[1] = u2;
         //return users;
 
-        return new User[] {u1, u2};
+        return new User[] {u1, u2,u3,u4};
     }
 }
