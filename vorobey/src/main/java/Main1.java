f6fe442464e53a218d71357a7c10fc8103e3586b
vorobeyDjack <vrba@i.ua>
2015-03-09 20:27:39 +0200

    creat method

--- vorobey/src/main/java/Main1.java
+++ vorobey/src/main/java/Main1.java
@@ -1,20 +1,52 @@
+import java.util.Scanner;
+
 /**
  * Created by user on 09.03.2015.
  */
 public class Main1 {
     public static void main(String[] args) {
-        showUser();
+        Scanner scanner = new Scanner(System.in);
+        checkUser();
+
+
+        while (scanner.hasNext()) {
+            switch (scanner.next()) {
+                case "1":
+                    showUser();
+                    break;
+                case "Exit":
+                    System.exit(0);
+
+                default:
+                    System.out.println("No such case");
+            }
+        }
 
     }
 
     public static void showUser() {
         User[] users = UsersUtil.getUsers();
         for (int i = 0; i < users.length; i++) {
-            System.out.print(users[i].name + "  " + users[i].secondName);
+            System.out.print(" " + users[i].name + "  " + users[i].secondName);
         }
+        System.out.println();
+
+    }
+
+    public static void checkUser() {
+        User[] users = UsersUtil.getUsers();
+        for (int i = 0; i < users.length; i++) {
+            if (users[i].name.equals(users[i].secondName)) {
+                System.out.println("Имя и Фамилия совпадают");
+            } else {
+                System.out.println("Не совпадают");
+            }
+        }
+        System.out.println();
+
+
     }
 
 }
-
 
 
