a0825fda81ea4c1cfa7545db07d78ab8e45f12dc
randriyanov <randriyanov@mail.ru>
2015-03-09 19:49:44 +0200

    new base course samples

--- src/main/java/lesson_1/Main.java
+++ src/main/java/lesson_1/Main.java
@@ -3,6 +3,14 @@ package lesson_1;
 
 public class Main {
     public static void main(String[] args) {
+        showUsers();
+    }
 
+    public static void showUsers() {
+        User[] users = UsersUtil.getUsers();
+        for (int i = 0; i < users.length; i++) {
+            System.out.print(users[i].name + " " + users[i].secondName);
+        }
+        System.out.println();
     }
 }
