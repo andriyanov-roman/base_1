d8244a0db8fd8f2f1a2f2ba4269aba9347167c84
job.dmy@gmail.com <rtem.iashin@gmail.com>
2015-03-09 20:59:48 +0200

    commit Mihael

--- mihael/src/main/java/hometask1/Main.java
+++ mihael/src/main/java/hometask1/Main.java
@@ -7,15 +7,24 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main (String [] args){
+        start();
+    }
+
+    public  static void start (){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter comman key" +
-                "1. Schow all users");
+        System.out.println("Enter comman key");
+        System.out.println("1. Schow all users");
+        System.out.println("2. Schow sovpadajuschie u odnogo usera");
+        System.out.println("3. Schow sovpadajuschie u vseh userov");
         while (scanner.hasNext()){
             switch (scanner.next()){
                 case "1":
                     showUsers();
-                break;
-
+                    break;
+                case "2":
+                    showSovUsers();
+                case "3":
+                    showObschFamiliiUsers();
                 case "Exit":
                     System.exit(0);
 
@@ -27,7 +36,9 @@ public class Main {
         //showUsers();
         showSovUsers();
 
+
     }
+
     public static void showUsers(){
         User[] users = UserUtil.getUsers();
         for (int i = 0; i < users.length; i++) {
@@ -39,12 +50,27 @@ public class Main {
     public static void showSovUsers(){
         User[] users = UserUtil.getUsers();
         for (int i = 0; i < users.length; i++) {
-            if(users[i].name== users[i].login || users[i].secondName ==users[i].login) {
+            if(users[i].name.equals(users[i].login)  || users[i].secondName.equals(users[i].login)) {
                 System.out.print(users[i].login + " " + users[i].name + " " + users[i].secondName);
             }
         }
         System.out.println();
     }
 
+    public static void showObschFamiliiUsers(){
+        User[] users = UserUtil.getUsers();
+        for (int i = 0; i < users.length; i++) {
+            for (int k = 0; k < users.length; k++) {
+
+                if(users[i].secondName.equals(users[k].secondName)) {
+                    System.out.println("i" + i + " k " + k);
+                    System.out.println("i user" + users[i].login + " " + users[i].name + " " + users[i].secondName);
+                    System.out.println("k user" + users[k].login + " " + users[k].name + " " + users[k].secondName);
+                }
+
+            }
+        }
+        System.out.println();
+    }
 
 }
