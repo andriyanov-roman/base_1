8dec51ee82428e3b28b2b29b3e1ae6ddbb4b100a
vorobeyDjack <vrba@i.ua>
2015-03-09 19:44:42 +0200

    sout users[]

--- vorobey/src/main/java/Main1.java
+++ vorobey/src/main/java/Main1.java
@@ -3,10 +3,16 @@
  */
 public class Main1 {
     public static void main(String[] args) {
-
+        showUser();
 
     }
 
+    public static void showUser() {
+        User[] users = UsersUtil.getUsers();
+        for (int i = 0; i < users.length; i++) {
+            System.out.print(users[i].name + "  " + users[i].secondName);
+        }
+    }
 
 }
 
