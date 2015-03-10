0c91e919cdcce700fa4def7127282ce7c7a4bbbf
vorobeyDjack <vrba@i.ua>
2015-03-10 23:25:31 +0200

    creating new classes

---
+++ vorobey/src/main/java/array/ArrayMethods.java
@@ -0,0 +1,38 @@
+package array;
+
+/**
+ * Created by Stas on 10.03.2015.
+ */
+public class ArrayMethods {
+    public static void arrayStart() {
+
+    }
+
+    public static void arrayChange() {
+
+    }
+
+    public static void arrayMin() {
+
+    }
+
+    public static void arrayMinBeforeHalf() {
+
+    }
+
+    public static void arrayMinMaxIminImax() {
+
+    }
+
+    public static void arraySummaLeftSummaRight() {
+
+    }
+
+    public static void arraySummaOtrizal() {
+
+    }
+
+    public static void arraySummaBeetwinMinMax() {
+
+    }
+}
