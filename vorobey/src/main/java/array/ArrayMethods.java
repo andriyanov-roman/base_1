dfa8f45fc35b9b821fb2d4d75f451eda1696165b
vorobeyDjack <vrba@i.ua>
2015-03-10 23:47:55 +0200

    creating new methods in Class

--- vorobey/src/main/java/array/ArrayMethods.java
+++ vorobey/src/main/java/array/ArrayMethods.java
@@ -1,18 +1,79 @@
 package array;
 
-/**
- * Created by Stas on 10.03.2015.
- */
+
+import java.util.Scanner;
+
 public class ArrayMethods {
     public static void arrayStart() {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            switch (sc.next()) {
+                case "1":
+
+                    break;
+                case "2":
+
+                    break;
+                case "3":
+
+                    break;
+                case "4":
+
+                    break;
+                case "5":
+
+                    break;
+                case "6":
+
+                    break;
+                case "7":
+
+                    break;
+
+
+            }
+        }
+
+
+    }
+
+    public static void arrayBeforeNull() {
+        int[] array = {-2, 5, 7, 0, -2, -7, 10, 1, 23, 3};
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                for (int k = 0; k < i; k++)// Вывод массива елементов,находяшихся перед 0
+                    System.out.print(" " + array[k]);
+            }
+
+        }
+
 
     }
 
     public static void arrayChange() {
 
+
     }
 
     public static void arrayMin() {
+        int[] array = {-2, 5, 7, 0, -2, -7, 10, 1, 23, 3};
+        int k, min = 500;
+
+        for (int i = 0; i < array.length; i++)
+            System.out.printf("%4d", array[i]);
+        System.out.println();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min)
+                min = array[i];
+        }
+        for (int i = 0; i < array.length; i++) {// Вывод массива до минимального элемента
+            if (array[i] == min) {
+                for (k = 0; k < i; k++)
+                    System.out.printf("%4d", array[k]);
+            }
+        }
+
 
     }
 
@@ -35,4 +96,13 @@ public class ArrayMethods {
     public static void arraySummaBeetwinMinMax() {
 
     }
+
+    public static void arrayShow() {
+        int[] array = {-2, 5, 7, 0, -2, -7, 10, 1, 23, 3};
+        for (int i = 0; i < array.length; i++)
+            System.out.print(array[i] + " ");
+
+    }
+
+
 }
