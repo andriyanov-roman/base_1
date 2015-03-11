4e19731ccd98a0759f59c380e8ba38e008af05fe
vorobeyDjack <vrba@i.ua>
2015-03-11 15:15:36 +0200

    create new methods in Class Methods

---
+++ vorobey/src/main/java/operator/Methods.java
@@ -0,0 +1,44 @@
+package operator;
+
+
+import java.util.Scanner;
+
+public class Methods {
+    public static void launch() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        while (sc.hasNext()) {
+            switch (sc.next()) {
+                case "1":
+
+                    break;
+                case "2":
+
+                    break;
+                case "3":
+
+                    break;
+                default:
+                    System.out.println("No such case!");
+
+            }
+        }
+
+    }
+
+    public static void max() {
+
+    }
+
+    public static void transfer() {
+
+    }
+
+    public static void maxOfFour() {
+
+    }
+
+
+}
