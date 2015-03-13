16fa891e5ba2a1e5924ce45fb73b525b0f7dc07c
mihrasik <job.dmy@gmail.com>
2015-03-13 15:47:05 +0200

    Mihael commit main

---
+++ mihael/src/main/java/hometask1/Employee.java
@@ -0,0 +1,13 @@
+package hometask1;
+
+/**
+ * Created by Администратор on 12.03.2015.
+ */
+public class Employee {
+    public Long id;
+    public String firstName;
+    public String lastName;
+    public int salary;
+
+
+}
