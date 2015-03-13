a8f32f4f1ef7c78626eaf14c0534cef633baa5fa
randriyanov <randriyanov@mail.ru>
2015-03-13 20:58:38 +0200

    updated for base course

--- src/main/java/base_examples/strings/StringExample.java
+++ src/main/java/base_examples/strings/StringExample.java
@@ -5,56 +5,38 @@ import java.util.Scanner;
 
 public class StringExample {
     public static void main(String[] args) {
-        int i = 0;
-        int[] array = {};
-        char[] hello = {'h', 'e', 'l', 'l', 'o'};
-        for (int j = 0; j < hello.length; j++) {
-            System.out.print(hello[j]);
-        }
-        System.out.println();
-        String str = "hello";
-        System.out.println(str);
-        String strOne = new String("hello");
-        System.out.println(strOne);
-        String strTwo = "hello";
-        System.out.println(str.equals(strOne));
-        System.out.println(str.equals(strTwo));
-        String strThree = "hello";
-        System.out.println(str + strOne + strTwo + strThree);
-        char a = str.charAt(0);
-        System.out.println(a);
-        boolean b = str.endsWith("lo");
-        System.out.println(b);
-        int ind = str.indexOf('h');
-        System.out.println(ind);
-        ind = str.indexOf("lm");
-        System.out.println(ind);
-        System.out.println("hello".equals("Hello"));
-        System.out.println("hello".equalsIgnoreCase("Hello"));
-        str = strOne.replace('h', 'Y');
-        System.out.println(str);
-        str = str.replaceAll("l", "bb");
-        System.out.println(str);
-        str = str.replaceFirst("bb", "l");
-        System.out.println(str);
-        str = str.substring(2);
-        System.out.println(str);
-        str = str.substring(2, 4);
-        System.out.println(str);
-        String test = "Lost Luggage is an action-video " +
-                "game developed and released, in 1982 " +
-                "for the Atari 2600 by the Texas-based studio Games by Apollo.";
-        String[] words = test.split("\\s");
-        for (int j = 0; j < words.length; j++) {
-            System.out.println(words[j]);
-        }
-        System.out.println(words[11]);
-        Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNext()) {
-            String[] pool = scanner.next().split(" ");
-            for (int j = 0; j < pool.length; j++) {
-                System.out.println(pool[i]);
+        String str = "Hugh Walpole abba (1884–1941) " +
+                "was a New aaa Zealand-born English novelist";
+        String[] words = str.split("\\s");
+        for (int i = 0; i < words.length; i++) {
+            char[] palindr;
+            palindr = words[i].toCharArray();
+            char[] p1 = new char[palindr.length / 2];
+            char[] p2 = new char[palindr.length / 2];
+            for (int j = 0; j < palindr.length / 2; j++) {
+                p1[j] = palindr[j];
+                p2[j] = palindr[palindr.length - 1 - j];
+            }
+            String pone = new String(p1); //преобразование честно подсмотрено в интернете
+            String ptwo = new String(p2); //и это тоже
+            if (pone.equalsIgnoreCase(ptwo) && words[i].length() > 2) {
+                System.out.println(words[i] + " is palindrome");
             }
         }
+        /*for (int i = 0; i < array.length; i++) {
+            char[] charArray = array[i].toCharArray();
+            char[] tempArray = new char[charArray.length];
+            for (int j = charArray.length - 1; j >= 0 ; j--) {
+                tempArray[charArray.length - 1 - j] = charArray[j];
+            }
+            String temp = new String(tempArray);
+            String polindrom = "";
+            for (int j = 0; j < array.length; j++) {
+                if(temp.equals(array[i])) {
+                    polindrom = array[i];
+                }
+            }
+            System.out.println(polindrom);
+        }*/
     }
 }
