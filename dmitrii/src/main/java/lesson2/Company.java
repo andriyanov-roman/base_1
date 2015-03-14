804a40d15e736747b5968e40d4031ec8ddb7f545
lisunovdv <lisunovdv@gmail.com>
2015-03-14 16:01:43 +0200

    Lesson Sat, 14/03/2015

--- dmitrii/src/main/java/lesson2/Company.java
+++ dmitrii/src/main/java/lesson2/Company.java
@@ -4,7 +4,14 @@ package lesson2;
  * Created by user on 14.03.2015.
  */
 public class Company {
-    public String name;
-
+    private String companyName;
     public Employee[] employees;
+
+    public void setCompanyName (String name){
+        this.companyName = name;
+    }
+
+    public String getCompanyName () {
+        return companyName;
+    }
 }
