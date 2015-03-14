2e8d5b5af84444a41a6383579b41fad67c7787a1
Aliso4ka1313 <Vasilevskaya9090@mail.ru>
2015-03-14 16:01:57 +0200

    classwork

---
+++ alisa/src/main/java/lessons/lesson_2/Main.java
@@ -0,0 +1,12 @@
+package lessons.lesson_2;
+
+/**
+ * Created by user on 14.03.2015.
+ */
+public class Main {
+    public static void main (String args[]){
+
+        CompanyUtil.start();
+
+    }
+}
