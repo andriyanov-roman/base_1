c195385900af08c5046899ea431b297c911fde74
lisunovdv <lisunovdv@gmail.com>
2015-03-14 15:35:43 +0200

    Lesson Sat, 14/03/2015

---
+++ dmitrii/src/main/java/lesson2/Company.java
@@ -0,0 +1,10 @@
+package lesson2;
+
+/**
+ * Created by user on 14.03.2015.
+ */
+public class Company {
+    public String name;
+
+    public Employee[] employees;
+}
---
+++ dmitrii/src/main/java/lesson2/Employee.java
@@ -0,0 +1,21 @@
+package lesson2;
+
+/**
+ * Created by user on 14.03.2015.
+ */
+public class Employee {
+    public String name;
+    public String surname;
+    public double salary;
+
+    public Employee() {
+
+    }
+
+    public Employee(String name, String surname, Double salary) {
+        this.name = name;
+        this.surname = surname;
+        this.salary = salary;
+    }
+}
+
