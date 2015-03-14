b269d39fb699c085ec8d661e4b4f336755ea380c
randriyanov <randriyanov@mail.ru>
2015-03-14 15:51:14 +0200

    updated for base course

--- src/main/java/lesson_2/Company.java
+++ src/main/java/lesson_2/Company.java
@@ -4,6 +4,22 @@ package lesson_2;
  * Created by Администратор on 14.03.2015.
  */
 public class Company {
-    public String companyName;
-    public Employee[] employees;
+    private String companyName;
+    private Employee[] employees;
+
+    public Employee[] getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Employee[] employees) {
+        this.employees = employees;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
 }
