b2bc82323cfc64ed84d7f9219f7c1e85029ad6e2
job.dmy@gmail.com <rtem.iashin@gmail.com>
2015-03-14 16:03:56 +0200

    commit Mihael

--- mihael/src/main/java/lesson2/Main.java
+++ mihael/src/main/java/lesson2/Main.java
@@ -4,12 +4,16 @@ package lesson2;
  * Created by user on 14.03.2015.
  */
 public class Main {
+   /*
     public static void main (String[] args){
+
         start();
 
     }
+
     public static void start(){
         CompanyUtil.showSearchName("P");
         CompanyUtil.showBiggestSallaries();
     }
+    */
 }
