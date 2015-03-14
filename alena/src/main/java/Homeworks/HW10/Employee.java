b3e258052a64a2cf6f6e46ab216ccecfad3d9896
Alena <alyonaklischenko@gmail.com>
2015-03-14 16:10:12 +0200

    alena first commit

--- alena/src/main/java/Homeworks/HW10/Employee.java
+++ alena/src/main/java/Homeworks/HW10/Employee.java
@@ -7,4 +7,14 @@ public class Employee {
     public String name;
     public String secondName;
     public double salary;
+
+    public Employee(String name, String secondName, double salary) {
+        this.name = name;
+        this.secondName = secondName;
+        this.salary = salary;
+    }
+
+    public Employee() {
+
+    }
 }
