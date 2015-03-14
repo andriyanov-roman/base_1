f576c0e5192abd57b880a646ec3a2cda49e9342f
b0gut <ruslanvankevich@gmail.com>
2015-03-14 16:08:51 +0200

    first test

--- ruslan/src/main/java/lesson_2/Company.java
+++ ruslan/src/main/java/lesson_2/Company.java
@@ -5,4 +5,19 @@ public class Company {
     public String companyName;
     public Employee[] employees;
 
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public Employee[] getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Employee[] employees) {
+        this.employees = employees;
+    }
 }
