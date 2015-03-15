4418789439c31230068b84c6e51b1bdcea33e0f1
Aliso4ka1313 <Vasilevskaya9090@mail.ru>
2015-03-15 04:38:53 +0400

    Employee

---
+++ alisa/src/main/java/lesson_1/Employee.java
@@ -0,0 +1,18 @@
+package lesson_1;
+
+/**
+ * Created by PC XP on 29.01.15.
+ */
+public class Employee {
+    private String name;
+    private String surname;
+    private double salary;
+
+
+    public Employee(String name, String surname, double salary){
+
+    }
+
+}
+
+
---
+++ alisa/src/main/java/lesson_1/EmployeeUtil.java
@@ -0,0 +1,19 @@
+package lesson_1;
+
+/**
+ * Created by PC XP on 29.01.15.
+ */
+public class EmployeeUtil {
+    public static Employee [] getEmployees() {
+    Employee e1 = new Employee ("Vasya", "Ivanow", 3000.00);
+    Employee e2 = new Employee ("Sania", "Petrov", 5000.00);
+    Employee e3 = new Employee ("Sergey", "Sidorov", 7000.00);
+    Employee e4 = new Employee ("Caley", "Ivanowa", 13000.00);
+    Employee e5 = new Employee ("Zhora", "Simonov", 33000.00);
+    Employee e6 = new Employee ("Jack", "Woker", 28000.00);
+    Employee e7 = new Employee ("Lora", "Smith", 75000.00);
+    Employee e8 = new Employee ("Sioma", "James", 300000.00);
+    Employee e9 = new Employee ("Marco", "Pantani", 5673000.00);
+        return new Employee [] {e1,e2,e3,e4,e5,e6,e7,e8,e9};
+    }
+}
