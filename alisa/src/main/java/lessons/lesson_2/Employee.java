0352bb03c0b90dd30671f1849ea220c051464cbc
Aliso4ka1313 <Vasilevskaya9090@mail.ry>
2015-03-16 18:33:01 +0200

    brediatina

---
+++ alisa/src/main/java/lessons/lesson_2/Employee.java
@@ -0,0 +1,44 @@
+package lessons.lesson_2;
+
+/**
+ * Created by admin on 16.03.15.
+ */
+public class Employee {
+
+    private String name;
+    private String surname;
+    private double salary;
+    private int age;
+    private String sex;
+
+    public String getName (){
+        return name;
+    }
+    public void setName (){
+        this.name = name;
+    }
+public String getSurname(){
+    return surname;
+}
+    public void setSurname(){
+        this.surname = surname;
+    }
+    public double getSalary(){
+        return salary;
+    }
+    public void  setSalary(){
+        this.salary = salary;
+    }
+    public String getSex(){
+        return sex;
+    }
+    public void setSex(){
+        this.sex = sex;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge (){
+        this.age = age;
+    }
+    }
