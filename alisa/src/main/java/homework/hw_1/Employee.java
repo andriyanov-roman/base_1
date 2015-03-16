71b54266c172aff535ab6a1ae52228a2339571b4
Aliso4ka1313 <Vasilevskaya9090@mail.ry>
2015-03-16 17:55:06 +0200

    emp

--- alisa/src/main/java/homework/hw_1/Employee.java
+++ alisa/src/main/java/homework/hw_1/Employee.java
@@ -7,16 +7,14 @@ public class Employee {
     public String name;
     public String surname;
     public double salary;
-    private int age;
-    private String sex;
 
 
-    public Employee(String name, String surname, double salary, int age, String sex){
-       this.name = name ;
-       this.surname = surname;
-        this.salary = salary;
-        this.age = age;
-       this.sex = sex;
+
+    public Employee(String a, String b, double c){
+       name = a ;
+       surname = b;
+        salary = c;
+
     }
 
 }
