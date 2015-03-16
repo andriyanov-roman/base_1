c5cc576cc52af370d7e51279ff8567c536b27b90
b0gut <ruslanvankevich@gmail.com>
2015-03-16 20:36:40 +0200

    first test

---
+++ ruslan/src/main/java/lesson_3/Company.java
@@ -0,0 +1,24 @@
+package lesson_3;
+
+
+public class Company {
+    private String companyName;
+//    private ArrayList<Employee> employees;
+//
+//    public ArrayList<Employee> getEmployees() {
+//        return employees;
+//    }
+//
+//    public void setEmployees(ArrayList<Employee> employees) {
+//        this.employees = employees;
+//    }
+//
+//    public void setCompanyName(String companyName) {
+//        this.companyName = companyName;
+//    }
+//
+//    public String getCompanyName() {
+//        return companyName;
+//    }
+//}
+}
---
+++ ruslan/src/main/java/lesson_3/Employee.java
@@ -0,0 +1,56 @@
+package lesson_3;
+
+
+public class Employee {
+    private String name;
+    private String secondName;
+    private double salary;
+    private int age;
+    private String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + secondName + " " + salary + " " + age + " " + gender ;
+
+    }
+}
