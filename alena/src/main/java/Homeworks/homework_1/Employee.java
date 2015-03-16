235c519c1c34880dbe943b240e7a5c6058e2ed9e
pechivo <alyonaklischenko@gmail.com>
2015-03-16 15:10:06 +0200

    something old2

---
+++ alena/src/main/java/Homeworks/homework_1/Employee.java
@@ -0,0 +1,16 @@
+package homeworks.homework1;
+
+/**
+ * Created by MacBook on 16.03.15.
+ */
+public class Employee {
+    public String name;
+    public String secondName;
+    public double salary;
+
+    public Employee(String a, String b, double c){
+        name = a ;
+        secondName = b;
+        salary = c;
+    }
+}
---
+++ alena/src/main/java/Homeworks/homework_1/User.java
@@ -0,0 +1,22 @@
+package Homeworks.homework_1;
+
+/**
+ * Created by MacBook on 16.03.15.
+ */
+public class User {
+    public long id;
+    public String name;
+    public String secondName;
+    public String login;
+    public String password;
+    public int age;
+
+    public User(long a, String b, String c, String d, String e, int f) {
+        id = a;
+        name = b;
+        secondName = c;
+        login = d;
+        password = e;
+        age = f;
+    }
+}
