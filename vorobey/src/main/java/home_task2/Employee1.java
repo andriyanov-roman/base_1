f037ea3600f1845942094d743189b89416474f45
vorobeyDjack <vrba@i.ua>
2015-03-18 15:58:26 +0200

    change methods to arrayList

--- vorobey/src/main/java/home_task2/Employee1.java
+++ vorobey/src/main/java/home_task2/Employee1.java
@@ -1,18 +1,18 @@
 package home_task2;
 
 
-public class Employee {
+public class Employee1 {
     public String name;
     public String secondName;
     public int salary;
     public int age;
     public String sex;
 
-    Employee() {
+    Employee1() {
 
     }
 
-    public Employee(String name, String secondName, int salary, int age, String sex) {
+    public Employee1(String name, String secondName, int salary, int age, String sex) {
 
         this.name = name;
         this.secondName = secondName;
--- vorobey/src/main/java/home_task2/EmployeeUtil.java
+++ vorobey/src/main/java/home_task2/EmployeeUtil.java
@@ -5,14 +5,12 @@ import home_task.Employee;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-/**
- * Created by Stas on 18.03.2015.
- */
+
 public class EmployeeUtil {
-    public static ArrayList<Employee> getEmployees() {
+    public static ArrayList<Employee1> getEmployees() {
 
 
-        Employee e1 = new Employee();
+        Employee1 e1=new Employee1();
 
         e1.name = "Roman";
         e1.secondName = "Andersson";
@@ -20,28 +18,28 @@ public class EmployeeUtil {
         e1.age = 24;
         e1.sex = "male";
 
-        Employee e2 = new Employee();
+        Employee1 e2=new Employee1();
         e2.name = "Vlad";
         e2.secondName = "Sokolov";
         e2.salary = 2500;
         e2.age = 37;
         e2.sex = "male";
 
-        Employee e3 = new Employee();
+        Employee1 e3=new Employee1();
         e3.name = "Vlad";
         e3.secondName = "Sokolov";
         e3.salary = 1300;
         e3.age = 18;
         e3.sex = "male";
 
-        Employee e4 = new Employee();
+        Employee1 e4=new Employee1();
         e4.name = "Maria";
         e4.secondName = "Elthton";
         e4.salary = 7000;
         e4.age = 56;
         e4.sex = "female";
 
-        Employee e5 = new Employee();
+        Employee1 e5=new Employee1();
         e5.name = "Ivan";
         e5.secondName = "Starper";
         e5.salary = 8000;
@@ -49,21 +47,21 @@ public class EmployeeUtil {
         e5.sex = "male";
 
 
-        Employee e6 = new Employee();
+        Employee1 e6=new Employee1();
         e6.name = "Vasiliy";
         e6.secondName = "Ytkin";
         e6.salary = 7000;
         e6.age = 33;
         e6.sex = "male";
 
-        Employee e7 = new Employee();
+        Employee1 e7=new Employee1();
         e7.name = "Nina";
         e7.secondName = "Volkova";
         e7.salary = 17000;
         e7.age = 30;
         e7.sex = "female";
 
-        Employee e8 = new Employee();
+        Employee1 e8=new Employee1();
         e8.name = "Anton";
         e8.secondName = "Neudachnik";
         e8.salary = 500;
@@ -71,7 +69,7 @@ public class EmployeeUtil {
         e8.sex = "male";
 
 
-        return new ArrayList<Employee>(Arrays.asList(e1, e2, e3, e4, e5, e6, e7, e8));
+        return new ArrayList<Employee1>(Arrays.asList(e1,e2,e3,e4,e5,e6,e7,e8));
 
 
     }
