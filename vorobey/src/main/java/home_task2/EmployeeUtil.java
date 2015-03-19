c65184a03af5b542e2e219a402ba2e188fb7fa8e
vorobeyDjack <vrba@i.ua>
2015-03-20 01:56:54 +0200

    add new private fields

--- vorobey/src/main/java/home_task2/EmployeeUtil.java
+++ vorobey/src/main/java/home_task2/EmployeeUtil.java
@@ -10,66 +10,64 @@ public class EmployeeUtil {
     public static ArrayList<Employee1> getEmployees() {
 
 
-        Employee1 e1=new Employee1();
+        Employee1 e1 = new Employee1();
 
         e1.setName("Sveta");
         e1.setSecondName("Andersson");
-        e1.
-        e1.age = 25;
-        e1.sex = "female";
+        e1.setSalary(2400);
+        e1.setAge(25);
+        e1.setSex("female");
 
-        Employee1 e2=new Employee1();
-        e2.name = "Vlad";
-        e2.secondName = "Sokolov";
-        e2.salary = 2500;
-        e2.age = 37;
-        e2.sex = "male";
+        Employee1 e2 = new Employee1();
+        e2.setName("Vlad");
+        e2.setSecondName("Sokolov");
+        e2.setSalary(2500);
+        e2.setAge(27);
+        e2.setSex("male");
 
-        Employee1 e3=new Employee1();
-        e3.name = "Vlad";
-        e3.secondName = "Sokolov";
-        e3.salary = 1300;
-        e3.age = 18;
-        e3.sex = "male";
+        Employee1 e3 = new Employee1();
+        e3.setName("Vlad");
+        e3.setSecondName("Sokolov");
+        e3.setSalary(1300);
+        e3.setAge(18);
+        e3.setSex("male");
 
-        Employee1 e4=new Employee1();
-        e4.name = "Maria";
-        e4.secondName = "Elthton";
-        e4.salary = 7000;
-        e4.age = 56;
-        e4.sex = "female";
+        Employee1 e4 = new Employee1();
+        e4.setName("Maria");
+        e4.setSecondName("Elthton");
+        e4.setSalary(7000);
+        e4.setAge(56);
+        e4.setSecondName("female");
 
-        Employee1 e5=new Employee1();
-        e5.name = "Ivan";
-        e5.secondName = "Starper";
-        e5.salary = 8000;
-        e5.age = 55;
-        e5.sex = "male";
-
-
-        Employee1 e6=new Employee1();
-        e6.name = "Vasiliy";
-        e6.secondName = "Ytkin";
-        e6.salary = 7000;
-        e6.age = 33;
-        e6.sex = "male";
-
-        Employee1 e7=new Employee1();
-        e7.name = "Nina";
-        e7.secondName = "Volkova";
-        e7.salary = 17000;
-        e7.age = 30;
-        e7.sex = "female";
-
-        Employee1 e8=new Employee1();
-        e8.name = "Anton";
-        e8.secondName = "Neudachnik";
-        e8.salary = 500;
-        e8.age = 37;
-        e8.sex = "male";
+        Employee1 e5 = new Employee1();
+        e5.setName("Ivan");
+        e5.setSecondName("Starper");
+        e5.setSalary(8000);
+        e5.setAge(60);
+        e5.setSecondName("male");
 
 
-        return new ArrayList<Employee1>(Arrays.asList(e1,e2,e3,e4,e5,e6,e7,e8));
+        Employee1 e6 = new Employee1();
+        e6.setName("Vasiliy");
+        e6.setSecondName("Ytkin");
+        e6.setAge(33);
+        e6.setSex("male");
+
+        Employee1 e7 = new Employee1();
+        e7.setName("Nina");
+        e7.setSecondName("Ohrenevsheva");
+        e7.setSalary(17000);
+        e7.setAge(23);
+        e7.setSex("female");
+
+        Employee1 e8 = new Employee1();
+        e8.setName("Anton");
+        e8.setSecondName("Neudachnik");
+        e8.setSalary(340);
+        e8.setAge(38);
+        e8.setSex("male");
+
+        return new ArrayList<Employee1>(Arrays.asList(e1, e2, e3, e4, e5, e6, e7, e8));
 
 
     }
