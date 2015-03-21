28faf25d09178042141ed1a9703877083f017cd3
lisunovdv <lisunovdv@gmail.com>
2015-03-21 14:37:00 +0200

    Lesson 4 --- 21/03/2015

--- dmitrii/src/main/java/lesson4/Main.java
+++ dmitrii/src/main/java/lesson4/Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by user on 21.03.2015.
@@ -13,9 +14,18 @@ public class Main {
         File file = new File("D:\\testDir\\test.txt");
         FileReader reader = new FileReader(file);
         BufferedReader buffer = new BufferedReader(reader);
+
+        ArrayList<String> STR = new ArrayList<>();
         String line;
         while( (line=buffer.readLine()) !=null){
+            String[] pool = line.split(" ");
+            for (int i = 0; i < pool.length; i++) {
+                STR.add(pool[i]);
+            }
             System.out.println(line);
+            for (int i = 0; i < 10; i++) {
+                System.out.println(STR.get(i));
+            }
         }
     }
 }
