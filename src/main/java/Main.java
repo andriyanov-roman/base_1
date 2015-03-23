a49f19d4c594907e219959bb436fd9f942c9e5b4
randriyanov <randriyanov@mail.ru>
2015-03-23 20:37:49 +0200

    updated for base course

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,18 +2,31 @@ import base_examples.objects.Human;
 import home_work_2.Employee;
 
 import java.io.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        LocalDate fromString = LocalDate.parse("2014-01-20");
+        LocalDateTime time = LocalDateTime.now();
+        System.out.println(time);
+        Date date = new Date();
+        System.out.println(fromString);
+        System.out.println(date);
+
+    }
+
+    public static void writeToFile() throws IOException  {
         FileWriter writer = new FileWriter("src/test_write.txt",true);
         writer.write(getEmployees().toString());
         writer.flush();
         writer.close();
-
     }
+
     public static ArrayList<Employee> getEmployees() throws IOException {
         File file = new File("D:\\work\\test.txt");
         FileReader reader = new FileReader(file);
