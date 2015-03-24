69345a5ea6eaf74993f4bac705a3e49759eb80fc
vorobeyDjack <vrba@i.ua>
2015-03-24 20:13:09 +0200

    create class Factory

---
+++ vorobey/src/main/java/home_task4/Factory.java
@@ -0,0 +1,27 @@
+package home_task4;
+
+
+public class Factory {
+    private String nameFactory;
+    private String location;
+
+
+    public String getNameFactory() {
+        return nameFactory;
+    }
+
+    public void setNameFactory(String nameFactory) {
+        this.nameFactory = nameFactory;
+    }
+
+    public String getLocation() {
+        return location;
+
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+
+}
