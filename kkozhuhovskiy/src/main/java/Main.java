74ef080169ec678435ac441fea717a3c3d471ef0
Kirill <k.kozhuhovskiy@gmail.com>
2015-03-28 14:10:41 +0200

    Homework 2 done

--- kkozhuhovskiy/src/main/java/Main.java
+++ kkozhuhovskiy/src/main/java/Main.java
@@ -1,12 +1,13 @@
 import homeWork1.StartUserProgram;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
  * Created by Kirill on 12.03.2015.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         StartUserProgram.start();
     }
 
