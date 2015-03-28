b4f999fd2fbc635457ec06486c477476f2e13202
Kirill <k.kozhuhovskiy@gmail.com>
2015-03-28 15:14:52 +0200

    new

---
+++ kkozhuhovskiy/src/main/java/homeWork1/CompanyUtil.java
@@ -0,0 +1,11 @@
+package homeWork1;
+
+/**
+ * Created by Kirill on 28.03.2015.
+ */
+public class CompanyUtil {
+
+    public static void addEmployeesToFile(){
+
+    }
+}
---
+++ kkozhuhovskiy/src/main/java/homeWork1/Manager.java
@@ -0,0 +1,13 @@
+package homeWork1;
+
+/**
+ * Created by Kirill on 28.03.2015.
+ */
+public class Manager {
+    private String name;
+    private String secondName;
+    private double salary;
+    private String gender;
+    private int age;
+    private String workingPlatform;
+}
