7f5ed4e905d10270be3d9f14186e3296c79bdc81
SamoshkinR-Tem <samoshkin-@ukr.net>
2015-03-28 16:01:15 +0200

    28/03/2015

--- artem/src/main/java/lessons/EmployeeUtil.java
+++ artem/src/main/java/lessons/EmployeeUtil.java
@@ -1,30 +1,27 @@
 package lessons;
 
 
+import entity.Admin;
 import entity.Employee;
+import homework.Methods;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class EmployeeUtil {
-    public static ArrayList<Employee> getEmployees() throws IOException {
-        File file = new File("D:\\base_1\\artem\\employees.txt");
-        FileReader reader = new FileReader(file);
-        BufferedReader buffer = new BufferedReader(reader);
-        ArrayList<Employee> employees = new ArrayList<Employee>();
-        String line;
-        while ((line = buffer.readLine()) != null) {
-            String[] pool = line.split(":");
-            Employee e = new Employee();
-            e.setName(pool[0]);
-            e.setSecondName(pool[1]);
-            e.setSalary(Double.valueOf(pool[2]));
-            e.setSex(pool[3]);
-            e.setAge(Integer.valueOf(pool[4]));
-            employees.add(e);
+    public static ArrayList<Employee> get() throws IOException {
+        File f = new File("artem/src/main/java/additionalFiles");
+        String[] position = f.list();
+
+        ArrayList allPositions = new ArrayList<>();
+        for (int i = 0; i <position.length; i++) {
+            ArrayList<String[]> strings = Methods.readFromFile("artem/src/main/java/additionalFiles/" + position[i], ":");
+
         }
-        return employees;
+        return allPositions;
     }
 }
--- artem/src/main/java/lessons/Main.java
+++ artem/src/main/java/lessons/Main.java
@@ -10,16 +10,6 @@ import java.util.ArrayList;
  */
 public class Main {
     public static void main(String[] args) throws IOException {
-        /*ArrayList<Employee> employees =  homework.EmployeeUtil.getEmployees();
-        for (int i = 0; i < employees.size(); i++) {
-            System.out.println(employees.get(i));
-        }
-        saveFile();*/
-    }
-    public static void saveFile()throws IOException{
-        FileWriter writer = new FileWriter("artem/src/main/java/entity/test",true);
-        writer.write("Ololol");
-        writer.flush();
-        writer.close();
+
     }
 }
