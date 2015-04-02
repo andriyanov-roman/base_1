1e4f3e12e67de86ff208a4d8810ff4427874e2e8
Aliso4ka1313 <Vasilevskaya9090@mail.ry>
2015-04-02 14:56:55 +0300

    scanner trouble

---
+++ alisa/src/main/java/lessons/lesson_2/CompanyMethods.java
@@ -0,0 +1,208 @@
+package lessons.lesson_2;
+
+import entity.Admin;
+import entity.Manager;
+import entity.Programmer;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Created by admin on 02.04.15.
+ */
+public class CompanyMethods {
+    private String companyName;
+    private Scanner scanner = new Scanner(System.in);
+    private ArrayList<Employee> employees = new ArrayList<>();
+    private ArrayList<Company> companies = new ArrayList<>();
+    public void start() throws IOException {
+
+        System.out.println("Enter command key: " +
+                "\n 0.Show all companies " +
+                "\n 1. Show an employee with MaxSalary " +
+                "\n 2.Sort a company  by salary " +
+                "\n 3.Sort a company  by age" +
+                "\n 4.Sort a company  by surname length" +
+                "\n 5.Add a new employee to Lenovo" +
+                "\n 6. Add a new employee to IBM" +
+                "\n 7.Add a new employee to Luxoft \n");
+        while (scanner.hasNext()) {
+            switch (scanner.next()) {
+                case "0":
+                    showCompanies();
+                    break;
+                case "1":
+                    getMaxSalary();
+                    break;
+                case "2":
+                    companyName = checkInput();
+                    sortBySalary(getCompByName(companyName));
+                    break;
+                case "3":
+                    companyName = checkInput();
+                    sortByAge(getCompByName(companyName));
+                    break;
+                case "4":
+                    companyName = checkInput();
+                    sortBySurnameLength(getCompByName(companyName));
+                    break;
+                case "5":
+                    createNewEmployee();
+                    writeToFile( "alisa/src/result/Lenovo.txt");
+                    CompanyUtil.readFromFile("alisa/src/result/Lenovo.txt", ":");
+                    System.out.println(employees.toString());
+                    break;
+                case "6":
+                    createNewEmployee();
+                    writeToFile( "alisa/src/result/IBM.txt");
+                    CompanyUtil.readFromFile("alisa/src/result/IBM.txt", ":");
+                    System.out.println(employees.toString());
+                    break;
+                case "7":
+                    createNewEmployee();
+                    writeToFile("alisa/src/result/Luxoft.txt");
+                    CompanyUtil.readFromFile("alisa/src/result/Luxoft.txt", ":");
+                    System.out.println(employees.toString());
+                    break;
+                case "Exit":
+                    System.exit(0);
+                default:
+                    System.out.println("No such case");
+            }
+        }
+    }
+
+
+    public void showCompanies()throws IOException  {
+    //    ArrayList<Company> companies = CompanyUtil.getCompanies();
+        for (int i = 0; i < companies.size(); i++) {
+            System.out.println(companies.get(i).getCompanyName());
+        }
+    }
+
+    public void getMaxSalary() throws IOException {
+       // ArrayList<Company> companies = CompanyUtil.getCompanies();
+        for (int i = 0; i < companies.size(); i++) {
+            Employee temp = companies.get(i).getEmployees().get(0);
+            for (int j = 0; j < companies.get(i).getEmployees().size(); j++) {
+                double salary = companies.get(i).getEmployees().get(j).getSalary();
+                if (temp.getSalary() < salary) {
+                    temp = companies.get(i).getEmployees().get(j);
+                }
+            }
+            System.out.println(companies.get(i).getCompanyName() + " " + temp.getName() + " " + temp.getSalary());
+        }
+    }
+
+    public String checkInput() {
+        System.out.println("Enter company name");
+        return scanner.next();
+    }
+
+    public Company getCompByName(String name) throws IOException {
+       // ArrayList<Company> companies = CompanyUtil.getCompanies();
+        Company company = companies.stream().filter(c -> c.getCompanyName().equals(name)).findAny().get();
+//        for (int i = 0; i < getCompanies().size(); i++) {
+//            if (name.equals(companies.get(i).getCompanyName())) {
+//                return companies.get(i);
+//            }
+//        }
+        return companies.stream().filter(c -> c.getCompanyName().equals(name)).findAny().get();
+    }
+
+
+    public static void sortBySalary(Company company) {
+        for (int i = 0; i < company.getEmployees().size(); i++) {
+            for (int j = 0; j < company.getEmployees().size() - 1; j++) {
+                if (company.getEmployees().get(j).getSalary() < company.getEmployees().get(j + 1).getSalary()) {
+                    Employee temp = company.getEmployees().get(j);
+                    company.getEmployees().set(j, company.getEmployees().get(j + 1));
+                    company.getEmployees().set((j + 1), temp);
+                }
+
+            }
+        }
+        System.out.println(company.getCompanyName());
+        for (int j = 0; j < company.getEmployees().size(); j++) {
+            System.out.println(company.getEmployees().get(j).getName() + " "
+                    + company.getEmployees().get(j).getSurname() + " "
+                    + company.getEmployees().get(j).getSalary());
+        }
+    }
+
+    public void sortByAge(Company company) {
+        for (int i = 0; i < company.getEmployees().size(); i++) {
+            for (int j = 0; j < company.getEmployees().size() - 1; j++) {
+                if (company.getEmployees().get(j).getAge() < company.getEmployees().get(j + 1).getAge()) {
+                    Employee temp = company.getEmployees().get(j);
+                    company.getEmployees().set(j, company.getEmployees().get(j + 1));
+                    company.getEmployees().set((j + 1), temp);
+                }
+
+            }
+        }
+        System.out.println(company.getCompanyName());
+        for (int j = 0; j < company.getEmployees().size(); j++) {
+            System.out.println(company.getEmployees().get(j).getName() + " "
+                    + company.getEmployees().get(j).getSurname() + " "
+                    + company.getEmployees().get(j).getAge());
+        }
+    }
+
+    public void sortBySurnameLength(Company company) {
+        for (int i = 0; i < company.getEmployees().size(); i++) {
+            for (int j = 0; j < company.getEmployees().size() - 1; j++) {
+                if (company.getEmployees().get(j).getSurname().equals(company.getEmployees().get(j + 1).getSurname())) {
+                    Employee temp = company.getEmployees().get(j);
+                    company.getEmployees().set(j, company.getEmployees().get(j + 1));
+                    company.getEmployees().set((j + 1), temp);
+                }
+
+            }
+        }
+        System.out.println(company.getCompanyName());
+        for (int j = 0; j < company.getEmployees().size(); j++) {
+            System.out.println(company.getEmployees().get(j).getName() + " "
+                    + company.getEmployees().get(j).getSurname());
+        }
+    }
+
+    private void createNewEmployee() {
+        Employee employee = new Employee();
+        System.out.println("Enter name");
+        employee.setName(scanner.next());
+        System.out.println("Enter surname");
+        employee.setSurname(scanner.next());
+        System.out.println("Enter salary");
+        double salary = Double.valueOf(scanner.next());
+        employee.setSalary(salary);
+        System.out.println("Enter age");
+        int age = Integer.valueOf(scanner.next());
+        employee.setAge(age);
+        System.out.println("Enter sex");
+        employee.setSex(scanner.next());
+        System.out.println("Do you want to continue? Y/N");
+        employees.add(employee);
+        if ("Y".equals(scanner.next())) {
+            createNewEmployee();
+        }
+    }
+
+    public void writeToFile(String path) {
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(path, true);
+            writer.write(employees.toString());
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+}
