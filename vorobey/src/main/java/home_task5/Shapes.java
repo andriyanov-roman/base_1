13eda84a3955a0a8e4edc018b829c90d64f75aaf
vorobeyDjack <vrba@i.ua>
2015-04-04 22:08:41 +0300

    create SuperClass Shapes

---
+++ vorobey/src/main/java/home_task5/Shapes.java
@@ -0,0 +1,42 @@
+package home_task5;
+
+public class Shapes {
+    public Shapes() {
+    }
+
+    Shapes(String name, double square, double perimetr) {
+        this.name = name;
+        this.square = square;
+        this.perimetr = perimetr;
+    }
+
+    private String name;
+    private double square;
+    private double perimetr;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSquare() {
+        return square;
+    }
+
+    public double getPerimetr() {
+        return perimetr;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSquare(double square) {
+        this.square = square;
+    }
+
+    public void setPerimetr(double perimetr) {
+        this.perimetr = perimetr;
+    }
+
+
+}
