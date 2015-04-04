35c48b06432b31481b7dee8a955a571f78a26a55
randriyanov <randriyanov@mail.ru>
2015-04-04 14:31:43 +0300

    added figures

---
+++ src/main/java/entity/figures/Figure.java
@@ -0,0 +1,34 @@
+package entity.figures;
+
+/**
+ * Created by Администратор on 04.04.2015.
+ */
+public class Figure {
+    private String name;
+    private double perimeter;
+    private double square;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPerimeter() {
+        return perimeter;
+    }
+
+    public void setPerimeter(double perimeter) {
+        this.perimeter = perimeter;
+    }
+
+    public double getSquare() {
+        return square;
+    }
+
+    public void setSquare(double square) {
+        this.square = square;
+    }
+}
---
+++ src/main/java/entity/figures/Triangle.java
@@ -0,0 +1,7 @@
+package entity.figures;
+
+/**
+ * Created by Администратор on 04.04.2015.
+ */
+public class Triangle extends Figure {
+}
