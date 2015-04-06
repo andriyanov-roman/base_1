89ea1ff794b720d9f200b5bc5dea2ef3a5527e2c
job.dmy@gmail.com <rtem.iashin@gmail.com>
2015-04-06 20:56:50 +0300

    Mihail

--- mihael/src/main/java/lection8/A.java
+++ mihael/src/main/java/lection8/A.java
@@ -4,7 +4,10 @@ package lection8;
  * Created by user on 06.04.2015.
  */
 public class A {
+    public static final int NEW_CONSTANT = 10;
+    public static int STAT = 10;
     public static void main(String [] args){
+        STAT = 10;
         A a = new B();
         a.doSome();
         a.doSomeNoStat();
