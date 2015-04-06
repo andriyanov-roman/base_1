021a163a3c30402dc65f093da67b79a665718b4a
lisunovdv <lisunovdv@gmail.com>
2015-04-06 20:22:58 +0300

    06-04-2015

---
+++ dmitrii/src/main/java/lesson060415/Main.java
@@ -0,0 +1,11 @@
+package lesson060415;
+
+/**
+ * Created by user on 06.04.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        A temp = new A();
+        A.AclInner second = temp.new AclInner();
+    }
+}
