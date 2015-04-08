e90c30007967907be5cf175cb5475c4d25a0c741
b0gut <ruslanvankevich@gmail.com>
2015-04-08 17:50:23 +0300

    home_commit_troubles

---
+++ ruslan/src/main/java/home_works/Company/company_util/files/UserUtil.java
@@ -0,0 +1,13 @@
+package home_works.company.company_util.files;
+
+import home_works.company.entities.company.User;
+
+import java.util.ArrayList;
+
+
+public class UserUtil {
+    public static ArrayList<User> getUsers() {
+        ArrayList<User> users = new ArrayList<>();
+        return users;
+    }
+}
