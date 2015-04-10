9f0c67ae89bf1041bb2f2382acd7b28d17b4743d
Aliso4ka1313 <Vasilevskaya9090@mail.ry>
2015-04-10 13:37:28 +0300

    figures

--- alisa/src/main/java/entity/figures/FourSquare.java
+++ alisa/src/main/java/entity/figures/FourSquare.java
@@ -4,15 +4,40 @@ package entity.figures;
  * Created by user on 04.04.2015.
  */
 public class FourSquare extends Figure {
-    private int side;
+    private double side;
 
-   /* @Override
-    public double getPerimetr() {
-        return side*4;
-    }*/
+
+
+    public FourSquare(String name, double perimeter, double square, double side) {
+        super(name, perimeter, square);
+        this.side = side;
+    }
+    public FourSquare(){
+
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double getPerimeter() {
+        super.getPerimeter();
+        return this.side*4;
+    }
 
     @Override
     public double getSquare() {
+        super.getSquare();
         return this.side * this.side;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
