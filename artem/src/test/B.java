a6dd1c87337b9c1bdfb8c4dd3ef9803dca193a96
SamoshkinR-Tem <samoshkin-@ukr.net>
2015-04-11 16:03:27 +0300

    Lesson

--- artem/src/test/B.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by user on 11.04.2015.
- */
-public class B {
-    public static void main(String[] args){
-        Class Class = new Class();
-        System.out.print(Class.toString());
-    }
-}
