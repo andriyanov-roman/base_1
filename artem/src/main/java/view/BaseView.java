cf1b870e78587670a587fe65eb7e1de1e1da26e8
samoshkinr-tem <samoshkin-@ukr.net>
2015-04-12 23:20:45 +0300

    12/04/2015

--- artem/src/main/java/view/BaseView.java
+++ artem/src/main/java/view/BaseView.java
@@ -11,15 +11,16 @@ public class BaseView {
     }
 
     public String startProgram (){
-        System.out.println("Hallo! This is Artem`s project. What kind of program do you want to see?"
-                + "\n 1 for Console Programs"
-                + "\n 2 for Swing Program"
-                + "\n 0 for Exit");
+        String s = "\n 1 for Console Programs" +
+                "\n 2 for Swing Program" +
+                "\n 0 for Exit";
+        System.out.println("Hallo! This is Artem`s project. What kind of program do you want to see?" + s);
         while (scanner.hasNext()){
             switch (scanner.next()){
                 case "1": return "console";
                 case "2": return "swing";
                 case "0": System.exit(0);
+                default: System.out.println("No such case! Try again:" + s);
             }
         }
         return null;
