e2baf7c878cef94cfb36c6b0753c5f0b44c82616
samoshkinr-tem <samoshkin-@ukr.net>
2015-04-12 10:47:26 +0300

    12/04/2015

--- artem/src/main/java/AppNavigator.java
+++ artem/src/main/java/AppNavigator.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 
 public class AppNavigator {
-    protected Scanner scanner = new Scanner(System.in);
+    private Scanner scanner = new Scanner(System.in);
 
     public void startProgram(){
         BaseView.startProgram();
