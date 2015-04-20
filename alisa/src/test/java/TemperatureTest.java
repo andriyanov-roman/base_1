cdb3055dd608bfd71ede71eb80830876079b4545
Aliso4ka1313 <Vasilevskaya9090@mail.ru>
2015-04-20 21:03:53 +0300

    wtf

---
+++ alisa/src/test/java/TemperatureTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import org.junit.Assert;
+
+/**
+ * Created by user on 20.04.2015.
+ */
+public class TemperatureTest {
+
+    double[] testArray = {1.3, 6.0, 9.0, -1.2, 0.4, 580};
+    @Test
+    public void testIfIsEmpty () throws Exception {
+     //   ISearchTemperature  search = new ISearchTemperature();
+      //  double result = search.closestToZero(testArray);
+       // Assert.assertNotEquals(0, result);
+
+
+    }
+}
