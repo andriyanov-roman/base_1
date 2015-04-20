359e6ecc3fb9a7875bc9b78d6b4bada7fbbbf8d2
mihrasik <job.dmy@gmail.com>
2015-04-20 20:27:11 +0300

    Mihael

--- mihael/src/main/java/lections/lesson11/Temperature.java
+++ mihael/src/main/java/lections/lesson11/Temperature.java
@@ -5,9 +5,50 @@ package lections.lesson11;
  */
 public class Temperature implements ISearchTemperature{
     double [] ts = {7,-10,13,8,4,-7.2,-12,-3.7,3.5,-9.6,6.5,-1.7,-6.2,7};
+    public static final  int MIN_TEMPERATURE = -273;
+    public static final  int MAX_TEMPERATURE = 5526;
+    //double [] ts = {7,-10,13,8,4,-7.2,-12,-3.7,3.5,-9.6,6.5,-1.7,-6.2,7,1.7};
+    //double [] ts = {7,-10,13,8,4,-7.2,-273,-3.7,5526,-9.6,6.5,-1.7,-6.2,7};
+    //double[] ts = {};
+    double minPlus;
+    double minMinus;
+    double result;
 
+    public  double [] getTs(){
+        return this.ts;
+    };
     @Override
-    public double closeToZero(double[] ts) {
-        return 0;
+    public  double closeToZero (double[] ts) throws Exception {
+        if(ts == null ||ts.length ==0) {
+            System.out.println("Array is empty. Return 0.");
+        }
+
+        for (int i = 0; i<ts.length; i++){
+            if((ts[i] >= MIN_TEMPERATURE) &&(ts[i] <= MAX_TEMPERATURE)){}
+            else throw new Exception("Exception- not in range");
+        }
+        minPlus = MAX_TEMPERATURE +1;
+        minMinus = MIN_TEMPERATURE -1;
+
+        for (int i = 0; i<ts.length; i++){
+
+            if((ts[i] < minPlus) &&(ts[i] > 0)){
+                minPlus = ts[i];
+            }
+            if((ts[i] > minMinus) &&(ts[i] < 0)){
+                minMinus = ts[i];
+
+            }
+        }
+
+        if(minPlus < Math.abs(minMinus)){
+            result = minPlus;
+        } else  if(minPlus == Math.abs(minMinus)){
+            result = minPlus;
+        } else result = minMinus;
+
+        return result;
     }
+
+
 }
--- mihael/src/test/java/lections/lesson11/TemperatureTest.java
+++ mihael/src/test/java/lections/lesson11/TemperatureTest.java
@@ -6,30 +6,46 @@ import org.junit.Test;
  * Created by Администратор on 20.04.2015.
  */
 public class TemperatureTest {
-    double [] ts3 = {7,-10,13,8,4,-7.2,-12,-3.7,3.5,-9.6,6.5,-1.7,-6.2,7};
-    double [] ts2 = {7,-10,13,8,4,-7.2,-273,-3.7,5527,-9.6,6.5,-1.7,-6.2,7};
+    public static final  int MIN_TEMPERATURE = -273;
+    public static final  int MAX_TEMPERATURE = 5526;
+    double [] ts3 = {7,-10,13,8,4,-7.2,-12,-3.7,3.5,-9.6,6.5,-1.7,-6.2,7,1.7};
+    double [] ts2 = {7,-10,13,8,4,-7.2,-273,-3.7,5526,-9.6,6.5,-1.7,-6.2,7};
     double[] ts = {};
     double minPlus;
     double minMinus;
+    double result;
     @Test
     public  void closeToZero() throws Exception {
-        if(ts.length ==0) {
+        if(ts == null ||ts.length ==0) {
             System.out.println("Array is empty. Return 0.");
         }
-        if(ts.length ==0) {
-            System.out.println("Array is empty. Return 0.");
-        }
+
         for (int i = 0; i<ts2.length; i++){
-            if((ts2[i] >= -273) &&(ts2[i] <= 5526)){}
+            if((ts2[i] >= MIN_TEMPERATURE) &&(ts2[i] <= MAX_TEMPERATURE)){}
             else throw new Exception("Exception- not in range");
         }
-        minPlus = 5527;
-        minMinus = -274;
+        minPlus = MAX_TEMPERATURE +1;
+        minMinus = MIN_TEMPERATURE -1;
+
         for (int i = 0; i<ts3.length; i++){
 
-            if((ts3[i] >= minPlus) &&(ts3[i] > 0)){}
-            else throw new Exception("Exception- not in range");
+            if((ts3[i] < minPlus) &&(ts3[i] > 0)){
+                minPlus = ts3[i];
+            }
+            if((ts3[i] > minMinus) &&(ts3[i] < 0)){
+                minMinus = ts3[i];
+
+            }
         }
+
+        if(minPlus < Math.abs(minMinus)){
+            result = minPlus;
+
+        } else  if(minPlus == Math.abs(minMinus)){
+            result = minPlus;
+        } else result = minMinus;
+
+        System.out.println(result);
 
     }
 }
