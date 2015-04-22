a60c1089e9b32a7c952b0970282673ef06c3d5de
matveu <matveu1990@gmail.com>
2015-04-22 19:53:08 +0300

    1

--- alisa/src/test/java/TemperatureImpl.java
+++ alisa/src/test/java/TemperatureImpl.java
@@ -4,21 +4,32 @@
 public class TemperatureImpl implements ISearchTemperature {
     double [] ts = {7,-10,13,8,4,-7.2,-12,-3.7,3.5,-9.6,6.5,-1.7,-6.2,7};
 
-   @Override
-    public double closestToZero(double[]ts)throws Exception{
-        double result = ts[0];
-        if (ts == null || ts.length ==0){
+    @Override
+    public double closestToZero(double[] ts) throws Exception {
+        if (ts == null || ts.length == 0) {
             return 0;
-
         }
-      // double result = ts [0];
-       for (int i = 0; i < ts.length; i++) {
-           if (ts[i]< MIN_TEMPERATURE || ts[i]> MAX_TEMPERATURE){
-            //   throw new Exception();
-           }
+        double result;
+        double maxNegative = MIN_TEMPERATURE;
+        double minPositive = ts[0];
+        for (int i = 0; i < ts.length; i++) {
+            if (ts[i] < MIN_TEMPERATURE || ts[i] > MAX_TEMPERATURE) {
+                throw new Exception();
+            }
+            if(ts[i] < 0 && ts[i] > maxNegative) {
+                maxNegative = ts[i];
+            }
+            if(ts[i] < minPositive  && ts[i] > 0) {
+                minPositive = ts[i];
+            }
+        }
 
-       }
-       return result;
+        if(Math.abs(maxNegative) == minPositive) {
+            result = minPositive;
+        } else {
+            result = minPositive;
+        }
+
+        return result;
     }
-
 }
--- alisa/src/test/java/TemperatureTest.java
+++ alisa/src/test/java/TemperatureTest.java
@@ -6,13 +6,13 @@ import org.junit.Assert;
  */
 public class TemperatureTest {
 
-    double[] testArray = {1.3, 6.0, 9.0, -1.2, 0.4, 580};
+    double[] testArray = {1.3, 6.0, 9.0, -1.2, 0.4, 580, -0.3, 0.2};
     @Test
     public void testIfIsEmpty () throws Exception {
-     TemperatureImpl  search = new TemperatureImpl();
+    /* TemperatureImpl  search = new TemperatureImpl();
       double result = search.closestToZero(testArray);
-       Assert.assertNotEquals(0, result);
+       Assert.assertNotEquals(0, result);*/
 
-
+        System.out.println(new TemperatureImpl().closestToZero(testArray));
     }
 }
