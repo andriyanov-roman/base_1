78e438365910a8f0de0874020583eb3cd8fdbc0e
randriyanov <randriyanov@mail.ru>
2015-04-25 14:59:02 +0300

    java fx example

---
+++ examples/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import fx.SimpleMenu;
+
+/**
+ * Created by Администратор on 25.04.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        new SimpleMenu().startProgram(args);
+    }
+}
--- examples/src/main/java/fx/SimpleMenu.java
+++ examples/src/main/java/fx/SimpleMenu.java
@@ -1,6 +1,8 @@
 package fx;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -10,6 +12,7 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -52,10 +55,20 @@ public class SimpleMenu extends Application {
         hbBtn.getChildren().add(btn);
         grid.add(hbBtn, 1, 4);
 
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 6);
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                actiontarget.setText(pwBox.getText());
+                actiontarget.setFill(Color.HOTPINK);
+                System.out.println("Test");
+            }
+        });
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
+    public void startProgram(String[] args) {
         launch(args);
     }
 }
