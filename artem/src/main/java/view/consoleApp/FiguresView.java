2b6ee978ca90f22194023f0e5974be0f1c858521
samoshkinr-tem <samoshkin-@ukr.net>
2015-04-27 15:57:08 +0300

    27/04/2015

--- artem/src/main/java/view/consoleApp/FiguresView.java
+++ artem/src/main/java/view/consoleApp/FiguresView.java
@@ -12,6 +12,7 @@ import java.util.Scanner;
  * Created by R-Tem on 21.04.2015.
  */
 public class FiguresView {
+    private Container container;
     protected Scanner scanner;
 
     public FiguresView(Scanner scanner) {
@@ -19,97 +20,78 @@ public class FiguresView {
     }
 
     public String menu(){
-        String menu = "\n '1' to add figure" +
+        String menu = " '1' to add figure" +
                 "\n '2' to get the figure with the biggest perimeter and square" +
                 "\n 'r' to return" +
                 "\n 'e' to Exit program";
-        System.out.println(" Welcome to Figures util! Create a container, please:");
-        System.out.print(" Enter parameters:" +
+        System.out.println(" Welcome to Figures util!");
+        try{
+            double check = container.getPerimeter();
+            System.out.println(menu);
+        }
+        catch (NullPointerException npe) {
+            setContainer();
+            System.out.println(" Done! Choose the action, please:\n" + menu);
+        }
+        while (scanner.hasNext()){
+            switch (scanner.next()){
+                case "1":
+                    addFigure();
+                    System.out.println(" Done! Choose the action, please:\n" + menu);
+                    break;
+                case "2": System.out.print(FiguresModule.biggestFig(container)); break;
+                case "r": return "return";
+                case "e": System.exit(0);
+                default:System.out.println("No such case! Try again:\n" + menu);
+            }
+        }
+        return null;
+    }
+    private void setContainer(){
+        System.out.print(" Create a container, please:" +
                 "\n width: ");
         double width = Double.valueOf(scanner.next());
         // здесь должен быть вызван валидатор, который проверяет на то, что это число
         System.out.print(" height: ");
         double contHeight = Double.valueOf(scanner.next());
         // здесь должен быть вызван валидатор
-        Container container = FiguresModule.createContainer(width, contHeight); // почему вызываемый метод обязан быть static???
-        System.out.println(" Done! Choose the action, please:" + menu);
-        while (scanner.hasNext()){
-            switch (scanner.next()){
-                case "1":
-                    container = addFigure(container);
-                    System.out.println(" Done! Choose the action, please:" + menu);
-                    break;
-                case "2": break;
-                case "r": return "return";
-                case "e": System.exit(0);
-                default:System.out.println("No such case! Try again:" + menu);
-            }
-        }
-        return null;
+        container = FiguresModule.createContainer(width, contHeight); // почему вызываемый метод обязан быть static???
     }
-    private Container addFigure(Container container){
+    private void addFigure(){
         String chooseFig = "\n '1' for circle" +
                 "\n '2' for square" +
                 "\n '3' for triangle" +
+                "\n 'r' to return" +
                 "\n 'e' to Exit program";
         System.out.println(" What kind of figure do you want to add: " + chooseFig);
         String name;
-        boolean b = true;
-        do {
-            switch (scanner.next()){
-                case "1":
-                    name = "circle";
-                    System.out.print("Enter radius, please: ");
-                    double radius = Double.valueOf(scanner.next());
-                    Circle circle = FiguresModule.createCircle(name, radius);
-                    container.setCircle(circle);
-                    toDoNext(container);
-                    b = false;
-                    break;
-                case "2":
-                    name = "square";
-                    System.out.print(" Enter side, please: ");
-                    double side = Double.valueOf(scanner.next());
-                    Square square = FiguresModule.createSquare(name, side);
-                    container.setFoursquare(square);
-                    toDoNext(container);
-                    b = false;
-                    break;
-                case "3":
-                    name = "triangle";
-                    System.out.print(" Enter side, please: ");
-                    side = Double.valueOf(scanner.next());
-                    System.out.print(" Enter height, please: ");
-                    double triangleHeight = Double.valueOf(scanner.next());
-                    Triangle triangle = FiguresModule.createTriangle(name, side, triangleHeight);
-                    container.setTriangle(triangle);
-                    toDoNext(container);
-                    b = false;
-                    break;
-                case "e": System.exit(0);
-                default: System.out.println("No such case! Try again:" + chooseFig);
-            }
-        } while (b);
-        return container;
-    }
-    private void toDoNext(Container container){
-        String s = "\n '1' to add one more figure" +
-                "\n '2' to go to Figures util menu" +
-                "\n 'e' to Exit program";
-        System.out.println(" Done! What do you want to do next?" + s);
-        boolean b = true;
-        do {
-            switch (scanner.next()){
-                case "1":
-                    addFigure(container);
-                    b = false;
-                    break;
-                case "2":
-                    b = false;
-                    break;
-                case "e": System.exit(0);
-                default: System.out.print("No such case! Try again, please:" + s);
-            }
-        } while (b);
+        switch (scanner.next()){
+            case "1":
+                name = scanner.next();
+                System.out.print("Enter radius, please: ");
+                double radius = Double.valueOf(scanner.next());
+                Circle circle = FiguresModule.createCircle(name, radius);
+                container.setCircle(circle);
+                break;
+            case "2":
+                name = scanner.next();
+                System.out.print(" Enter side, please: ");
+                double side = Double.valueOf(scanner.next());
+                Square square = FiguresModule.createSquare(name, side);
+                container.setFoursquare(square);
+                break;
+            case "3":
+                name = scanner.next();
+                System.out.print(" Enter side, please: ");
+                side = Double.valueOf(scanner.next());
+                System.out.print(" Enter height, please: ");
+                double triangleHeight = Double.valueOf(scanner.next());
+                Triangle triangle = FiguresModule.createTriangle(name, side, triangleHeight);
+                container.setTriangle(triangle);
+                break;
+            case "r": menu();
+            case "e": System.exit(0);
+            default: System.out.println("No such case! Try again:" + chooseFig);
+        }
     }
 }
