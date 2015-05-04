e8d70112fa7c02ab4e1bb197ac18ce46e3ab02ba
Alena <alyonaklischenko@gmail.com>
2015-05-04 20:55:07 +0300

    yeap

--- alena/src/main/java/entity/figures/Circle.java
+++ alena/src/main/java/entity/figures/Circle.java
@@ -4,4 +4,13 @@ package entity.figures;
  * Created by user on 04.04.2015.
  */
 public class Circle extends Figure {
+    @Override
+    public double getPerimetr() {
+        return 0;
+    }
+
+    @Override
+    public double getSquare() {
+        return 0;
+    }
 }
