8f42dcbe9860c908cefdb730347d4f24c27d30b9
SamoshkinR_Tem <tKtrnhjy1610-19>
2015-05-09 16:01:15 +0300

    09.05.2015

---
+++ artem/src/test/java/onTheLesson/ReadRegEx.java
@@ -0,0 +1,29 @@
+package onTheLesson;
+
+import org.junit.Test;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Programmer on 09.05.2015.
+ */
+public class ReadRegEx {
+    @Test
+    public void readRegExp() throws IOException {
+        File file = new File("C:\\Users\\Programmer\\IdeaProjects\\base_1\\artem\\src\\main\\resourses\\test\\text.txt");
+        FileReader reader = new FileReader(file);
+        BufferedReader buffer = new BufferedReader(reader);
+        String regExp = "\\d{3}\\/\\d{3}\\/\\d{4}";
+        Pattern pattern = Pattern.compile(regExp);
+        String line;
+        while ((line = buffer.readLine()) != null) {
+            Matcher matcher = pattern.matcher(line);
+            if(matcher.find()) {
+                System.out.println(line);
+            }
+        }
+    }
+}
