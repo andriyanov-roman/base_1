91474f79536827205b4b2136fd343b90726aec27
Programmer <vrba@i.ua>
2015-05-11 19:18:20 +0300

    change something

--- vorobey/src/main/java/lesson_today/ClassRegular.java
+++ vorobey/src/main/java/lesson_today/ClassRegular.java
@@ -9,23 +9,23 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ClassRegular {
-    public static void main(String[] args) throws IOException{
-        File file=new File("vorobey\\src\\main\\java\\test.txt");
-        FileReader fileReader=new FileReader(file);
-        BufferedReader bufferedReader=new BufferedReader(fileReader);
-        String regExp="(([a-zA-Z0-9]){1,}([\\s]){0,})*";
-        Pattern pattern=Pattern.compile(regExp);
-        Matcher matcher=pattern.matcher("yghtrhr34535      jdbfhwWREFWE324324   434343 hbhbhHVYGCFY");
-        boolean result =matcher.matches();
-        System.out.println("result "+result);
+    public static void main(String[] args) throws IOException {
+        File file = new File("vorobey\\src\\main\\java\\test.txt");
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String regExp = "(([a-zA-Z0-9]){1,}([\\s]){0,})*";
+        Pattern pattern = Pattern.compile(regExp);
+        Matcher matcher = pattern.matcher("yghtrhr34535      jdbfhwWREFWE324324   434343 hbhbhHVYGCFY");
+        boolean result = matcher.matches();
+        System.out.println("result " + result);
         String line;
-        /*while ((line=bufferedReader.readLine())!=null){
-            Matcher matcher=pattern.matcher(line);
-            boolean rezult =matcher.matches();
-            if(matcher.find()){
-                //System.out.println(line);
-                System.out.println("rezyltat "+rezult);
+        while ((line = bufferedReader.readLine()) != null) {
+            Matcher matcher1 = pattern.matcher(line);
+            boolean rezult = matcher.matches();
+            if (matcher.find()) {
+                System.out.println(line);
+                System.out.println("result " + result);
             }
-        }*/
+        }
     }
 }
