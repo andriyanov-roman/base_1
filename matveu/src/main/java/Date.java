d9b5b566f76fcc07f98ebc767046ffc84dc701dc
matveu <matveu1990@gmail.com>
2015-05-11 20:30:06 +0300

    регулярка

---
+++ matveu/src/main/java/Date.java
@@ -0,0 +1,20 @@
+import apps.employees.Employee;
+
+import java.util.Calendar;
+
+/**
+ * Created by Programmer on 11.05.2015.
+ */
+public class Date {
+    public static void main(String[] args){
+
+        Employee e1 = new Employee("Grisha", "Popov", "man", 25, 10000.0);
+        Calendar calendar=Calendar.getInstance();
+
+
+
+
+
+
+    }
+}
