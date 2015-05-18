4ad468db80bf43708d94acf640858d42df567777
Aliso4ka1313 <Vasilevskaya9090@mail.ry>
2015-05-18 17:08:12 +0300

    download button

--- mihael/src/main/java/programs/views/ProgramsFXView.java
+++ mihael/src/main/java/programs/views/ProgramsFXView.java
@@ -25,28 +25,35 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.swing.*;
 
 /**
  * Created by Администратор on 08.05.2015.
  */
 public class ProgramsFXView extends CommonFXView{
-    private int windowsWidth = 600;
-    private int windowsHeight = 800;
+    private int windowsWidth = 300;
+    private int windowsHeight = 300;
 
-    private Scene createScene(){
+   /* private Scene createScene(){
         //Group root = new Group();
         Scene scene = new Scene(createGrid(), windowsWidth,windowsHeight);
         return scene;
-    }
-    private  GridPane createGrid(){
+    }*/
+    private  void createGrid(){
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
-        grid.setGridLinesVisible(true);
+        //grid.setGridLinesVisible(true);
+        Scene scene = new Scene(grid, windowsWidth,windowsHeight);
 
         Text scenetitle = new Text("Add an employee");
         scenetitle.setFont(Font.font("Arial", FontWeight.NORMAL, 20));
@@ -81,22 +88,81 @@ public class ProgramsFXView extends CommonFXView{
 
 
         Button btn = new Button("Save");
-        btn.defaultButtonProperty().bind(btn.focusedProperty());
+        //btn.defaultButtonProperty().bind(btn.focusedProperty());
         HBox hbBtn = new HBox(10);
         hbBtn.setAlignment(Pos.CENTER);
         hbBtn.getChildren().add(btn);
         grid.add(hbBtn, 1, 6);
 
+        Button download = new Button ("Download");
+        HBox hbdownload = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_LEFT);
+        hbBtn.getChildren().add(download);
+        grid.add(hbBtn, 1, 6);
+
+
         final Text actionTargetField = new Text();
         grid.add(actionTargetField, 1, 7);
+        download.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    loadFiles();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
-        return  grid;
+    }
+
+
+public void loadFiles() throws Exception{
+    try {
+        FileOutputStream outputStream = null;
+        ArrayList<String> urls = new ArrayList<>(loadUrls());
+        for (int i = 0; i < urls.size(); i++) {
+            URL url = new URL(urls.get(i));
+            InputStream is = url.openStream();
+            outputStream = new FileOutputStream(new File(i + ".mp3"));
+            byte[] buffer = new byte[2048];
+            int length;
+            while ((length = is.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, length);
+            }
+            outputStream.close();
+        }
+    } catch (MalformedURLException e) {
+        e.printStackTrace();
+    }
+}
+    public static Set<String> loadUrls() {
+        Set<String> urls = new HashSet<>();
+        try {
+            URL url = new URL("http://www.ex.ua/82091884");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line;
+            Pattern pattern = Pattern.compile("href='([^']+)");
+            while ((line = reader.readLine()) != null) {
+                Matcher matcher = pattern.matcher(line);
+                if (matcher.find()) {
+                    if (matcher.group(1).contains("/get/")) {
+                        urls.add("http://www.ex.ua" + matcher.group(1));
+                    }
+                }
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return urls;
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Main Programs Menu");
-        primaryStage.setScene(createScene());
+        //primaryStage.setScene(createScene());
 
 
         primaryStage.show();
