348e691e84de4735c3a3bbfadaa5f24a36b2286a
pechivo <570707zz>
2015-05-18 18:59:31 +0300

    =)

--- alena/src/main/java/carFactory/FactoryController.java
+++ alena/src/main/java/carFactory/FactoryController.java
@@ -1,11 +1,24 @@
 package carFactory;
 
+import entity.Car;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Created by lbondarenko on 08.05.2015.
  */
 public class FactoryController {
+    private Scanner scanner = new Scanner(System.in);
+    private ArrayList<Car> cars = new ArrayList<>();
+
 
     public static void letsBegin (){
+        System.out.println ("Выберите действие: " +
+        "\n 0. Совершить заказ" +
+                "\n 1. Просмотреть все заказы " +
+        "\n 2. Показать самую дорогую машину" +
+        "\n 3. Показать машины одного цвета" );
 
     }
 }
