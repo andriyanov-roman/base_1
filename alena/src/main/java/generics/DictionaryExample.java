3e16bb63cb08f5cf3ea839c012ccfd7ec3fce98a
pechivo <570707zz>
2015-05-18 20:52:14 +0300

    =)

---
+++ alena/src/main/java/generics/DictionaryExample.java
@@ -0,0 +1,28 @@
+package generics;
+
+/**
+ * Created by User on 18.05.2015.
+ */
+public class DictionaryExample {
+    public static String uaWord;
+
+
+
+
+    public static String enWord;
+
+
+    public static String getEnWord() {
+        return enWord;
+    }
+
+    public static String getUaWord() {
+        return uaWord;
+    }
+    public static void setEnWord(String enWord) {
+        DictionaryExample.enWord = enWord;
+    }
+
+    public static void setUaWord(String uaWord) {
+        DictionaryExample.uaWord = uaWord;}
+}
