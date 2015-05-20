1bf5ebf4984948756cb6ebc949bf842d79f6e1bd
pechivo <570707zz>
2015-05-20 19:01:36 +0300

    =)

--- alena/src/main/java/carFactory/FactoryController.java
+++ alena/src/main/java/carFactory/FactoryController.java
@@ -2,6 +2,7 @@ package carFactory;
 
 import entity.Car;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -13,12 +14,28 @@ public class FactoryController {
     private ArrayList<Car> cars = new ArrayList<>();
 
 
-    public static void letsBegin (){
-        System.out.println ("Выберите действие: " +
-        "\n 0. Совершить заказ" +
+    public void letsBegin() throws IOException {
+        System.out.println("Выберите действие: " +
+                "\n 0. Совершить заказ" +
                 "\n 1. Просмотреть все заказы " +
-        "\n 2. Показать самую дорогую машину" +
-        "\n 3. Показать машины одного цвета" );
-
+                "\n 2. Показать самую дорогую машину" +
+                "\n 3. Показать машины одного цвета" +
+                "\n 4. Показать машины за январь");
+        while (scanner.hasNext()) {
+            switch (scanner.next()) {
+                case "0":
+                    break;
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "Exit":
+                    System.exit(0);
+            }
+        }
     }
 }
