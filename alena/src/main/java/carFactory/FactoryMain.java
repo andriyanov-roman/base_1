b5b4d985f9debb5709d18fdb51e0ac3d6f567e59
pechivo <570707zz>
2015-05-20 19:23:41 +0300

    =)

--- alena/src/main/java/carFactory/FactoryMain.java
+++ alena/src/main/java/carFactory/FactoryMain.java
@@ -1,10 +1,12 @@
 package carFactory;
 
+import java.io.IOException;
+
 /**
  * Created by user on 11.04.2015.
  */
 public class FactoryMain {
-    public static void main(String[] args) {
-        FactoryController.letsBegin();
+    public static void main(String[] args) throws IOException{
+        new FactoryController().letsBegin();
     }
 }
