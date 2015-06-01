ac4463b8e363c2fb71133f8fa75a0f21c85301bc
randriyanov <randriyanov@mail.ru>
2015-06-01 20:54:52 +0300

    write object example

---
+++ examples/src/main/java/cons/company/Company.java
@@ -0,0 +1,14 @@
+package cons.company;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Created by User on 01.06.2015.
+ */
+public class Company {
+    private Collection<Employee> employees;
+
+
+}
