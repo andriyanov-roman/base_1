42da3e050147aaffb8d326310928f6ce2cc3b31a
R-Tem <samoshkin-@ukr.net>
2015-06-06 23:28:01 +0300

    6.06.2015

---
+++ artem/src/main/java/entities/mvc/CommonModel.java
@@ -0,0 +1,7 @@
+package entities.mvc;
+
+/**
+ * Created by R-Tem on 17.05.2015.
+ */
+public class CommonModel {
+}
--- artem/src/test/java/home_task_7/task_7_part1/Sentence.java
+++
@@ -1,5 +0,0 @@
-package home_task_7.task_7_part1;
-
-public class Sentence extends Text {
-
-}
