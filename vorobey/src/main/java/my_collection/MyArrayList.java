bfc939141a27a96f6b5da607cca4d1334c33b991
Programmer <vrba@i.ua>
2015-06-06 13:20:09 +0300

    Create my ArrayList

---
+++ vorobey/src/main/java/my_collection/MyArrayList.java
@@ -0,0 +1,41 @@
+package my_collection;
+
+public class MyArrayList<T> {
+    private int size = 0;
+    private Object[] elements = {};
+
+    public MyArrayList() {
+        elements = new Object[10];
+    }
+
+    public void add(T element) throws Exception {
+        elements[size] = element;
+        size++;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return (T) elements[index];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public static void main(String[] args) throws Exception {
+        MyArrayList<Integer> list = new MyArrayList<Integer>();
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        int element = list.get(0);
+        System.out.println("This element = " + element);
+
+        int size = list.size();
+        System.out.println("List size = " + size);
+    }
+
+}
+
+
