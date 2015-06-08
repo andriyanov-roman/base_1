fa38ddabd6280e8c7baaa8db1dcb73ac4af44e98
User <Vasilevskaya9090@mail.ru>
2015-06-08 21:15:47 +0300

    xml empl

--- alisa/src/main/java/ReadXMLFile.java
+++ alisa/src/main/java/ReadXMLFile.java
@@ -1,4 +1,5 @@
 import entity.Company;
+import entity.Employee;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -10,6 +11,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by User on 08.06.2015.
@@ -42,18 +44,24 @@ public class ReadXMLFile {
             Element companyNameElement = (Element)dList.item(0);
             String companyName = companyNameElement.getElementsByTagName("companyname").item(0).getTextContent();
             company.setCompanyName(companyName);
+            company.setEmployees(new ArrayList<>());
             NodeList nList = getDoc().getElementsByTagName("employee");
             System.out.println("----------------------------");
             for (int temp = 0; temp < nList.getLength(); temp++) {
                 Node nNode = nList.item(temp);
                 System.out.println("\nCurrent Element :" + nNode.getNodeName());
                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Employee e = new Employee();
                     Element eElement = (Element) nNode;
-                    System.out.println("Employee id : " + eElement.getAttribute("id"));
-                    System.out.println("Name : " + eElement.getElementsByTagName("name").item(0).getTextContent());
+                    e.setName(eElement.getElementsByTagName("name").item(0).getTextContent());
+                    //System.out.println("Employee id : " + eElement.getAttribute("id"));
+                   /* System.out.println("Name : " + eElement.getElementsByTagName("name").item(0).getTextContent());
                     System.out.println("Surname : " + eElement.getElementsByTagName("surname").item(0).getTextContent());
                     System.out.println("Salary : " + eElement.getElementsByTagName("salary").item(0).getTextContent());
-                    System.out.println("Age : " + eElement.getElementsByTagName("age").item(0).getTextContent());
+                    System.out.println("Age : " + eElement.getElementsByTagName("age").item(0).getTextContent());*/
+                   e.setSalary(Double.parseDouble(eElement.getElementsByTagName("salary").item(0).getTextContent()));
+                    company.getEmployees().add(e);
+                    System.out.println(company);
                 }
             }
         } catch (Exception e) {
