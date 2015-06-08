e1f8b927851b7bf005434a1e4ad5aa8a7564ecc5
Programmer <vrba@i.ua>
2015-06-08 19:49:59 +0300

    create xml file

--- vorobey/src/main/java/xml/ReadXmlFile.java
+++ vorobey/src/main/java/xml/ReadXmlFile.java
@@ -13,26 +13,26 @@ import java.io.File;
 public class ReadXmlFile {
     public static void main(String argv[]) {
         try {
-            File fXmlFile = new File("/Users/mkyong/staff.xml");
+            File fXmlFile = new File("/vorobey/company.xml");
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(fXmlFile);
-            //optional, but recommended
-            //read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+
             doc.getDocumentElement().normalize();
             System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-            NodeList nList = doc.getElementsByTagName("staff");
-            System.out.println("----------------------------");
+            NodeList nList = doc.getElementsByTagName("company");
+            System.out.println("********************");
             for (int temp = 0; temp < nList.getLength(); temp++) {
                 Node nNode = nList.item(temp);
                 System.out.println("\nCurrent Element :" + nNode.getNodeName());
                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                     Element eElement = (Element) nNode;
-                    System.out.println("Staff id : " + eElement.getAttribute("id"));
-                    System.out.println("First Name : " + eElement.getElementsByTagName("firstname").item(0).getTextContent());
-                    System.out.println("Last Name : " + eElement.getElementsByTagName("lastname").item(0).getTextContent());
-                    System.out.println("Nick Name : " + eElement.getElementsByTagName("nickname").item(0).getTextContent());
-                    System.out.println("Salary : " + eElement.getElementsByTagName("salary").item(0).getTextContent());
+
+                    System.out.println("Name and Secondname: " + eElement.getElementsByTagName("name").item(0).getTextContent());
+                    System.out.println("Telephone : " + eElement.getElementsByTagName("telephone").item(0).getTextContent());
+                    System.out.println("Address : " + eElement.getElementsByTagName("address").item(0).getTextContent());
+                    System.out.println("Country : " + eElement.getElementsByTagName("country").item(0).getTextContent());
+                    System.out.println("City : " + eElement.getElementsByTagName("City").item(0).getTextContent());
                 }
             }
         } catch (Exception e) {
