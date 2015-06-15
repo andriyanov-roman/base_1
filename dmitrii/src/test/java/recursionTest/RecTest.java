4db9c781830665e11cea99cc6a359e82dd3581f0
lisunovdv <lisunovdv@gmail.com>
2015-06-15 21:04:58 +0300

    Recursion

--- dmitrii/src/test/java/recursionTest/RecTest.java
+++ dmitrii/src/test/java/recursionTest/RecTest.java
@@ -3,15 +3,18 @@ package recursionTest;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by Programmer on 15.06.2015.
  */
 public class RecTest {
     private int[] array = {1,2,3,4,5,6,7,8};
+    int j = 0;
 
     @Test
-    public void testRun (){
+    public void testRun () throws IOException {
+        testShow();
         showLeftHalf(0, array.length / 2);
         System.out.println("=============");
         showLeftHalf(array.length / 2, array.length);
@@ -24,6 +27,13 @@ public class RecTest {
         System.out.println("======Show files =======");
         showFiles("D:\\");
     }
+
+    private void testShow() {
+        if (j < array.length/2){
+
+        }
+    }
+
     public  void showLeftHalf (int i, int end) {
         if (i < end){
             System.out.println(array[i]);
@@ -42,11 +52,14 @@ public class RecTest {
         if (arg <= 1) return 1;
         else return arg * fakt( arg - 1 );
     }
-    public void showFiles(String startFolder){
+    public void showFiles(String startFolder) throws IOException {
         File f = new File(startFolder);
         if (f.isDirectory()){
-            System.out.println("");
-        }
+            System.out.println("<DIR>"+f.getPath());
+            for (File subFile : f.listFiles()){
+                showFiles(subFile.getCanonicalPath());
+            }
+        } else System.out.println(f.getCanonicalPath());
 
     }
 }
