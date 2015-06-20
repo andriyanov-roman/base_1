c32ed14f79f48276b0f90d9d119ccecff5f7b1e9
randriyanov <randriyanov@mail.ru>
2015-06-20 15:09:14 +0300

    task added

---
+++ examples/src/main/java/B.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Programmer on 20.06.2015.
+ */
+public class B extends A {
+
+    public void doSomething(Number n) {
+        System.out.println("from b number");
+    }
+
+    public void doSomething(Integer i) {
+        System.out.println("from b intger");
+    }
+}
