b52f4634a6fddce2cc3cc34633e888b683edc65b
randriyanov <randriyanov@mail.ru>
2015-06-20 10:03:59 +0300

    database added

---
+++ examples/src/main/java/db/ConnectorDB.java
@@ -0,0 +1,19 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+/**
+ * Created by user on 6/20/2015.
+ */
+public class ConnectorDB {
+    public static Connection getConnection() throws SQLException {
+        ResourceBundle resource = ResourceBundle.getBundle("database");
+        String url = resource.getString("db.url");
+        String user = resource.getString("db.user");
+        String pass = resource.getString("db.password");
+        return DriverManager.getConnection(url, user, pass);
+    }
+}
