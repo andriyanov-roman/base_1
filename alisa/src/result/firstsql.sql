35bfdba71578812dc7dfd240c600141bc339a857
User <Vasilevskaya9090@mail.ru>
2015-06-20 15:10:38 +0300

    sql

---
+++ alisa/src/result/firstsql.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE mydb;
+CREATE TABLE employee (
+	id MEDIUMINT NOT NULL AUTO_INCREMENT,
+    name_employee VARCHAR(100) NOT NULL,
+    second_name VARCHAR(100) NOT NULL,
+    age SMALLINT,
+    PRIMARY KEY(id)employee
+);
