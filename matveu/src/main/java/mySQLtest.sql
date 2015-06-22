ad97e2a79a209110ee69620e7959065d44e31ef4
Igor Matviichuk <matveu1990@gmail.com>
2015-06-22 16:47:43 +0300

    SQL

---
+++ matveu/src/main/java/mySQLtest.sql
@@ -0,0 +1,6 @@
+#insert into employee (name, secondname, sex, age, salary) values ('Igor','Matviichuk','man',25,200000);
+#select * from employee;
+#select name,secondname,age from employee where idemployee=2;
+#update employee set name='Olga',secondname='Lygovaya',age=37,salary=1100,sex='woman' where idemployee=17;
+#select * from employee where name='Valera';
+#delete from employee where idemployee=18;
