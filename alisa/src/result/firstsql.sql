ea980e14e9793569faaf7706b9be15891ea387cd
Aliso4ka1313 <Vasilevskaya9090@mail.ry>
2015-06-22 17:44:09 +0300

    Insert query

--- alisa/src/result/firstsql.sql
+++ alisa/src/result/firstsql.sql
@@ -1,8 +1,22 @@
 CREATE DATABASE mydb;
-CREATE TABLE employee (
-	id MEDIUMINT NOT NULL AUTO_INCREMENT,
-    name_employee VARCHAR(100) NOT NULL,
-    second_name VARCHAR(100) NOT NULL,
-    age SMALLINT,
-    PRIMARY KEY(id)employee
-);
+CREATE TABLE Employees (
+	EmployeeID MEDIUMINT NOT NULL AUTO_INCREMENT,
+    EmployeeName VARCHAR(100) NOT NULL,
+    EmployeeSecondName VARCHAR(100) NOT NULL,
+    Age SMALLINT,
+    PRIMARY KEY(EmployeeID)
+); 
+INSERT INTO Employees (EmployeeName, EmployeeSecondName, Age)
+VALUES ('John','Smith', 30),
+	   ('Jack','Woker', 59),
+       ('Antonio','Moreno', 27),
+       ('Ana','Schmidt', 30),
+       ('Thomas','Hardy', 30);
+
+UPDATE  Employees
+SET Age = 35, Age = 24 WHERE  EmployeeName  = 'Ana', EmployeeID  = 5;
+
+
+SELECT * FROM Employees; 
+
+
