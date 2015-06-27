58a17bca88339edb4d5b2fbb2998c20c657a0f7d
lisunovdv <lisunovdv@gmail.com>
2015-06-27 15:11:16 +0300

    sql

---
+++ dmitrii/src/main/java/lessons/sql/EmployeeDaoImpl.java
@@ -0,0 +1,33 @@
+package lessons.sql;
+
+import java.util.List;
+
+/**
+ * Created by Programmer on 27.06.2015.
+ */
+public class EmployeeDaoImpl implements GenericDao<EmployeeSQL> {
+    @Override
+    public List<EmployeeSQL> getEntities() {
+        return null;
+    }
+
+    @Override
+    public EmployeeSQL getEntity() {
+        return null;
+    }
+
+    @Override
+    public void update(EmployeeSQL employeeSQL) {
+
+    }
+
+    @Override
+    public void deleteEntity(EmployeeSQL employeeSQL) {
+
+    }
+
+    @Override
+    public void insert(EmployeeSQL employeeSQL) {
+
+    }
+}
